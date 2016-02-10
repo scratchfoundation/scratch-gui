ba63247e9d23bf1fc554e4e4dd54247e4994f5fb
Ray Schamp <ray@scratch.mit.edu>
2016-02-10 14:43:10 -0500

    Fix comments for Scratch generators

--- generators/javascript/event.js
+++ generators/javascript/event.js
@@ -2,7 +2,7 @@
  * @license
  * Visual Blocks Language
  *
- * Copyright 2012 Google Inc.
+ * Copyright 2016 Google Inc.
  * https://developers.google.com/blockly/
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,7 +19,7 @@
  */
 
 /**
- * @fileoverview Generating JavaScript for math blocks.
+ * @fileoverview Generating JavaScript for event blocks.
  * @author q.neutron@gmail.com (Quynh Neutron)
  */
 'use strict';

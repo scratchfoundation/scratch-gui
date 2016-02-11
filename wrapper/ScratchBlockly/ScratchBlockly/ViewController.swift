68ce2f96de41f60d7ec64754c92df573efd5f205
Tim Mickel <tim.mickel@gmail.com>
2016-02-11 14:32:51 -0500

    Add iOS wrapper and compiled playgrounds

---
+++ wrapper/ScratchBlockly/ScratchBlockly/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    var webview : WKWebView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.webview = WKWebView()
+        self.view = self.webview
+        
+        // If a URL is provided in the app settings, put the WebView there
+        // Otherwise, put it at the app bundle's www index
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let wwwSource = defaults.stringForKey("source_preference")
+        let bundleSource = NSBundle.mainBundle().pathForResource("horizontal_playground", ofType: "html", inDirectory: "playgrounds")
+
+        let url = (wwwSource != nil && wwwSource!.characters.count > 0) ? NSURL(string:wwwSource!) : NSURL.fileURLWithPath(bundleSource!)
+        let req = NSURLRequest(URL:url!)
+        webview!.loadRequest(req)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+

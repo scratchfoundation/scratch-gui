9cfe992b5ae57d8dfc3da0ecf02259d0c51a4423
Tim Mickel <tim.mickel@gmail.com>
2016-02-29 16:20:19 -0500

    Remove block transparency during drag

--- core/css.js
+++ core/css.js
@@ -189,8 +189,8 @@ Blockly.Css.CONTENT = [
   '}',
 
   '.blocklyDragging>.blocklyPath {',
-    'fill-opacity: .8;',
-    'stroke-opacity: .8;',
+    // 'fill-opacity: .8;',
+    // 'stroke-opacity: .8;',
   '}',
 
   '.blocklyDisabled>.blocklyPath {',

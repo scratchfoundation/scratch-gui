0336aa0347c6a404d903efefd0243b5db288b4e9
Tim Mickel <tim.mickel@gmail.com>
2016-04-13 17:22:41 -0400

    Remove width/height from blocklyDragSurface (#214)
    This was causing issue on iOS Safari when the SVG was exceeding the size of the window

--- core/css.js
+++ core/css.js
@@ -196,8 +196,6 @@ Blockly.Css.CONTENT = [
     'left: 0;',
     'right: 0;',
     'bottom: 0;',
-    'width: 100%;',
-    'height: 100%;',
     'overflow: visible !important;',
     'z-index: 5000;', /* Always display on top */
     '-webkit-backface-visibility: hidden;',

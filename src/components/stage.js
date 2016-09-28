e9801c8697d0d970c45562e19215cf2282dc717a
Ray Schamp <ray@scratch.mit.edu>
2016-09-28 17:35:03 -0400

    Set default stage size

--- src/components/stage.js
+++ src/components/stage.js
@@ -6,17 +6,23 @@ class Stage extends React.Component {
             <canvas
                 className="scratch-stage"
                 ref={this.props.stageRef}
+                width={this.props.width}
+                height={this.props.height}
             />
         );
     }
 }
 
 Stage.propTypes = {
-    stageRef: React.PropTypes.func
+    stageRef: React.PropTypes.func,
+    width: React.PropTypes.number,
+    height: React.PropTypes.number
 };
 
 Stage.defaultProps = {
-    stageRef: function () {}
+    stageRef: function () {},
+    width: 480,
+    height: 360
 };
 
 module.exports = Stage;

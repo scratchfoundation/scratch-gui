347971ce7282c200ecde5f1f115feee10354e818
Ray <ray@scratch.mit.edu>
2016-11-04 19:47:22 +0000

    Build for 845d533a84164d57ae5480bad117b4b38ad46041

--- gui.js
+++ gui.js
@@ -30617,7 +30617,7 @@ webpackJsonp([0],[
 	                style: {
 	                    position: 'absolute',
 	                    top: 0,
-	                    right: 470,
+	                    right: 500,
 	                    bottom: 0,
 	                    left: 0
 	                }

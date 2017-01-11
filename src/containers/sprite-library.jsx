26bfe9560feb7113c94977a38faa58c80fe59531
Ray Schamp <ray@scratch.mit.edu>
2017-01-11 18:58:46 -0500

    Static-ify sprite library

--- src/containers/sprite-library.jsx
+++ src/containers/sprite-library.jsx
@@ -1,7 +1,8 @@
 const bindAll = require('lodash.bindall');
 const React = require('react');
 const VM = require('scratch-vm');
-const MediaLibrary = require('../lib/media-library');
+
+const spriteLibraryContent = require('../lib/libraries/sprites.json');
 
 const LibaryComponent = require('../components/library/library.jsx');
 
@@ -9,47 +10,16 @@ class SpriteLibrary extends React.Component {
     constructor (props) {
         super(props);
         bindAll(this, [
-            'handleItemSelect',
-            'setData',
-            'setSpriteData'
+            'handleItemSelect'
         ]);
-        this.state = {
-            data: [],
-            spriteData: {}
-        };
-    }
-    componentWillReceiveProps (nextProps) {
-        if (nextProps.visible && this.state.data.length === 0) {
-            this.props.mediaLibrary.getMediaLibrary('sprite', this.setData);
-        }
-    }
-    setData (data) {
-        this.setState({data: data});
-        for (const sprite of data) {
-            this.props.mediaLibrary.getSprite(sprite.md5, this.setSpriteData);
-        }
-    }
-    setSpriteData (md5, data) {
-        this.setState({
-            spriteData: Object.assign({}, this.state.spriteData, {[md5]: data})
-        });
     }
     handleItemSelect (item) {
-        const spriteData = JSON.stringify(this.state.spriteData[item.json]);
-        this.props.vm.addSprite2(spriteData);
+        this.props.vm.addSprite2(JSON.stringify(item.json));
     }
     render () {
         return (
             <LibaryComponent
-                data={Object.keys(this.state.spriteData).map(libraryKey => {
-                    const libraryItem = this.state.spriteData[libraryKey];
-                    return {
-                        name: libraryItem.objName,
-                        md5: libraryItem.costumes[0].baseLayerMD5,
-                        json: libraryKey
-                    };
-                })}
-                mediaLibrary={this.props.mediaLibrary}
+                data={spriteLibraryContent}
                 title="Sprite Library"
                 visible={this.props.visible}
                 onItemSelected={this.handleItemSelect}
@@ -60,7 +30,6 @@ class SpriteLibrary extends React.Component {
 }
 
 SpriteLibrary.propTypes = {
-    mediaLibrary: React.PropTypes.instanceOf(MediaLibrary),
     onRequestClose: React.PropTypes.func,
     visible: React.PropTypes.bool,
     vm: React.PropTypes.instanceOf(VM).isRequired

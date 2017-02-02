e8fdd3b871d1bef83185a5abba7664aaafd7ba2b
Ray Schamp <ray@scratch.mit.edu>
2017-02-02 14:40:04 -0500

    Fix config for Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var autoprefixer = require('autoprefixer');
-var combineLoaders = require('webpack-combine-loaders');
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -19,50 +18,50 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: '[name].js'
     },
+    externals: {
+        React: 'react',
+        ReactDOM: 'react-dom'
+    },
     module: {
-        externals: {
-            React: 'react',
-            ReactDOM: 'react-dom'
-        },
-        loaders: [{
+        rules: [{
             test: /\.jsx?$/,
             loader: 'babel-loader',
             include: path.resolve(__dirname, 'src'),
-            query: {
+            options: {
                 plugins: ['transform-object-rest-spread'],
-                presets: ['es2015', 'react']
+                presets: [['es2015', {modules: false}], 'react']
             }
         },
         {
             test: /\.css$/,
-            loader: combineLoaders([{
-                loader: 'style'
+            use: [{
+                loader: 'style-loader'
             }, {
-                loader: 'css',
-                query: {
+                loader: 'css-loader',
+                options: {
                     modules: true,
                     importLoaders: 1,
                     localIdentName: '[name]_[local]_[hash:base64:5]',
                     camelCase: true
                 }
             }, {
-                loader: 'postcss'
-            }])
+                loader: 'postcss-loader',
+                options: {
+                    plugins: function () {
+                        return [
+                            autoprefixer({
+                                browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']
+                            })
+                        ];
+                    }
+                }
+            }]
         },
         {
             test: /\.svg$/,
             loader: 'svg-url-loader?noquotes'
-        },
-        {
-            test: /\.json$/,
-            loader: 'json-loader'
         }]
     },
-    postcss: [
-        autoprefixer({
-            browsers: ['last 3 versions', 'Safari >= 8', 'iOS >= 8']
-        })
-    ],
     plugins: [
         new webpack.DefinePlugin({
             'process.env.BASE_PATH': '"' + (process.env.BASE_PATH || '/') + '"',
@@ -82,10 +81,7 @@ module.exports = {
     ].concat(process.env.NODE_ENV === 'production' ? [
         new webpack.optimize.UglifyJsPlugin({
             include: /\.min\.js$/,
-            minimize: true,
-            compress: {
-                warnings: false
-            }
+            minimize: true
         })
     ] : [])
 };

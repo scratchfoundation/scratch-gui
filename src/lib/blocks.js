d3965210b9b395379fcb2becfb29d7219863938d
TheBrokenRail <connor24nolan@live.com>
2017-02-10 18:25:24 -0500

    Update blocks.js

--- src/lib/blocks.js
+++ src/lib/blocks.js
@@ -2,14 +2,14 @@ const ScratchBlocks = require('scratch-blocks');
 
 module.exports = function (vm) {
 
-    const jsonForMenuBlock = function (name, menuOptionsFn, colors) {
+    const jsonForMenuBlock = function (name, menuOptionsFn, colors, static) {
         return {
             message0: '%1',
             args0: [
                 {
                     type: 'field_dropdown',
                     name: name,
-                    options: menuOptionsFn
+                    options: staic.concat(menuOptionsFn)
                 }
             ],
             inputsInline: true,
@@ -32,23 +32,47 @@ module.exports = function (vm) {
     const backdropsMenu = function () {
         return vm.runtime.targets[0].sprite.costumes.map(costume => [costume.name, costume.name]);
     };
+    
+    const spriteMenu = function () {
+        var i = 0;
+        var sprites = [];
+        var k = 0;
+        for (i = 0; i < vm.runtime.targets.length) {
+            if (vm.runtime.targets[i].isOriginal == true) {
+                if (vm.runtime.targets[i].isStae == false) {
+                    var sprites[k] = [];
+                    sprites[k][0] = vm.runtime.targets[k].sprite.name;
+                    sprites[k][1] = vm.runtime.targets[k].sprite.name;
+                    k++;
+                }
+            }
+        }
+        return sprites;
+    }
 
     const soundColors = ScratchBlocks.Colours.sounds;
 
     const looksColors = ScratchBlocks.Colours.looks;
+    
+    const motionColors = ScratchBlocks.Colours.motion;
 
     ScratchBlocks.Blocks.sound_sounds_menu.init = function () {
-        const json = jsonForMenuBlock('SOUND_MENU', soundsMenu, soundColors);
+        const json = jsonForMenuBlock('SOUND_MENU', soundsMenu, soundColors, []);
         this.jsonInit(json);
     };
 
     ScratchBlocks.Blocks.looks_costume.init = function () {
-        const json = jsonForMenuBlock('COSTUME', costumesMenu, looksColors);
+        const json = jsonForMenuBlock('COSTUME', costumesMenu, looksColors, []);
         this.jsonInit(json);
     };
 
     ScratchBlocks.Blocks.looks_backdrops.init = function () {
-        const json = jsonForMenuBlock('BACKDROP', backdropsMenu, looksColors);
+        const json = jsonForMenuBlock('BACKDROP', backdropsMenu, looksColors, []);
+        this.jsonInit(json);
+    };
+    
+    ScratchBlocks.Blocks.motion_pointtowards_menu = function () {
+        const json = jsonForMenuBlock('TOWARDS', spriteMenu, motionColors, []);
         this.jsonInit(json);
     };
 

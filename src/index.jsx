e9d33f83881a66357de6641ab7e746887d7a91ba
Ray Schamp <ray@scratch.mit.edu>
2017-04-24 12:06:24 -0400

    Stop clobbering loading projects with default data
    If the app is loading with an initial project id, don't try to load the default project. Previously, this data would load on top of the project being loaded.
    
    Does not fix the issue of blocks not loading in the blocks pane, which appears to be separate.

--- src/index.jsx
+++ src/index.jsx
@@ -19,7 +19,7 @@ class App extends React.Component {
         this.updateProject = this.updateProject.bind(this);
         this.state = {
             projectId: null,
-            projectData: JSON.stringify(ProjectLoader.DEFAULT_PROJECT_DATA)
+            projectData: this.fetchProjectId().length ? null : ProjectLoader.DEFAULT_PROJECT_DATA
         };
     }
     componentDidMount () {
@@ -49,6 +49,7 @@ class App extends React.Component {
         }
     }
     render () {
+        if (this.state.projectData === null) return null;
         return (
             <GUI
                 basePath={this.props.basePath}

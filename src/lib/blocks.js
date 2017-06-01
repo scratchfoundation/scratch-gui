be590c499a3ebd4bc51e322b300bc0981c050d97
Paul Kaplan <pkaplan@media.mit.edu>
2017-06-01 14:20:05 -0400

    Update where the getCheckboxState function lives on scratch-blocks

--- src/lib/blocks.js
+++ src/lib/blocks.js
@@ -131,7 +131,7 @@ module.exports = function (vm) {
         this.jsonInit(json);
     };
 
-    ScratchBlocks.Blocks.getCheckboxState = function (blockId) {
+    ScratchBlocks.VerticalFlyout.getCheckboxState = function (blockId) {
         const monitoredBlock = vm.runtime.monitorBlocks._blocks[blockId];
         return monitoredBlock ? monitoredBlock.isMonitored : false;
     };

2abb0a40ec4fe30ad6e89f48c7cbb2eab83aa8f3
Susie Sahim <bogusred@paperdemon.com>
2017-06-13 14:15:50 -0700

    Got one test passing. yay!

--- test/unit/green-flag.test.jsx
+++ test/unit/green-flag.test.jsx
@@ -1,14 +1,20 @@
 /* eslint-env jest */
 const React = require('react');
 const {shallow} = require('enzyme');
-// const GreenFlag = require('../../src/components/green-flag/green-flag');
 const GreenFlag = require('../../src/containers/green-flag');
 const renderer = require('react-test-renderer');
+const VM = require('scratch-vm');
 
 describe('GreenFlag', () => {
+  let vm;
+  beforeEach(() => {
+    // TODO(bogusred): Ask scratch team their thoughts on mocking the VM.
+    vm = new VM();
+  });
+
   test('renders active state', () => {
     const component = renderer.create(
-        <GreenFlag active={true}/>
+        <GreenFlag active={true} vm={vm}/>
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
@@ -16,7 +22,7 @@ describe('GreenFlag', () => {
   test('triggers onClick when active', () => {
     const onClick = jest.fn();
     const component = shallow(
-        <GreenFlag active={true} onClick={onClick}/>
+        <GreenFlag active={true} onClick={onClick} vm={vm}/>
     );
     component.find('img').simulate('click');
     expect(onClick).toHaveBeenCalled();

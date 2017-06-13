ad2ab75d6ee90f6cc3e552dd6a9588d099a8e6e7
Susie Sahim <bogusred@paperdemon.com>
2017-06-13 13:44:34 -0700

    WIP getting a react test working. Throws syntax error on svgs so need to maybe configure jest to use existing webpack config instead which probably properly handles svgs. - added additional plugins to get tests working

--- test/unit/green-flag.test.jsx
+++ test/unit/green-flag.test.jsx
@@ -1,6 +1,24 @@
 /* eslint-env jest */
-// TODO: Import green flag and write tests.
+const React = require('react');
+const {shallow} = require('enzyme');
+// const GreenFlag = require('../../src/components/green-flag/green-flag');
+const GreenFlag = require('../../src/containers/green-flag');
+const renderer = require('react-test-renderer');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+describe('GreenFlag', () => {
+  test('renders active state', () => {
+    const component = renderer.create(
+        <GreenFlag active={true}/>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test('triggers onClick when active', () => {
+    const onClick = jest.fn();
+    const component = shallow(
+        <GreenFlag active={true} onClick={onClick}/>
+    );
+    component.find('img').simulate('click');
+    expect(onClick).toHaveBeenCalled();
+  });
 });

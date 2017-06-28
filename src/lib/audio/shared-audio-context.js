52441bbb516c3ff8dbee01f990d21907a9244fac
Andrew Sun͈̮ <as-com@users.noreply.github.com>
2017-06-28 18:59:04 -0400

    Fix AudioContext issue in Safari

--- src/lib/audio/shared-audio-context.js
+++ src/lib/audio/shared-audio-context.js
@@ -1,5 +1,5 @@
 // Wrap browser AudioContext because we shouldn't create more than one
-const AUDIO_CONTEXT = new AudioContext();
+const AUDIO_CONTEXT = new (AudioContext || webkitAudioContext)();
 
 module.exports = function () {
     return AUDIO_CONTEXT;

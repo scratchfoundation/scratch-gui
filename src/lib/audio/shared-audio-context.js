5162c6c175d467a3fec7abd40bb512d66cb6aa6f
Andrew Sun͈̮ <as-com@users.noreply.github.com>
2017-06-29 10:08:52 -0400

    Satisfy linter

--- src/lib/audio/shared-audio-context.js
+++ src/lib/audio/shared-audio-context.js
@@ -1,5 +1,5 @@
 // Wrap browser AudioContext because we shouldn't create more than one
-const AUDIO_CONTEXT = new (AudioContext || webkitAudioContext)();
+const AUDIO_CONTEXT = new (AudioContext || window.webkitAudioContext)();
 
 module.exports = function () {
     return AUDIO_CONTEXT;

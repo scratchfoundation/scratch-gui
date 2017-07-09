f5bbdc03123bfa71e1f0b6e6a14e55d1a77ceba5
TheBrokenRail <connor24nolan@live.com>
2017-07-09 12:15:46 -0400

    Update sprite-selector-item.jsx

--- src/components/sprite-selector-item/sprite-selector-item.jsx
+++ src/components/sprite-selector-item/sprite-selector-item.jsx
@@ -16,6 +16,7 @@ const SpriteSelectorItem = props => (
                 [styles.isSelected]: props.selected
             }
         )}
+        width={this.props.width}
         onClick={props.onClick}
     >
         {props.selected ? (
@@ -45,7 +46,11 @@ SpriteSelectorItem.propTypes = {
     name: PropTypes.string.isRequired,
     onClick: PropTypes.func,
     onDeleteButtonClick: PropTypes.func,
-    selected: PropTypes.bool.isRequired
+    selected: PropTypes.bool.isRequired,
+    width: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.string
+    ])
 };
 
 module.exports = SpriteSelectorItem;

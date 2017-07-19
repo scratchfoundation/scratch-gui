af6973759077bf159b2fead7c215af3dccf9e1d5
chrisgarrity <chrisg@media.mit.edu>
2017-07-19 12:04:53 -0400

    Add language selection widget to menubar
    Added language-selector component and container. Replaced lib/intl.jsx with reducers/intl.js and added updateIntl to handle language changes.
    
    Moved languages.json into src

--- src/reducers/intl.js
+++ src/reducers/intl.js
@@ -1,5 +1,26 @@
+import {addLocaleData} from 'react-intl';
 import {updateIntl as superUpdateIntl} from 'react-intl-redux';
-import {messages} from '../../locale/messages.json';
+import languages from '../languages.json';
+import messages from '../../locale/messages.json';
 
-export {intlReducer as default} from 'react-intl-redux';
-export const updateIntl = locale => superUpdateIntl(locale, messages[locale] || messages.en);
+export {IntlProvider, intlReducer as default} from 'react-intl-redux';
+
+Object.keys(languages).forEach(locale => {
+    // TODO: will need to handle locales not in the default intl - see www/custom-locales
+    const data = require(`react-intl/locale-data/${locale}`);
+    addLocaleData(data);
+});
+
+// start with English
+export const intlInitialState = {
+    intl: {
+        defaultLocale: 'en',
+        locale: 'en',
+        messages: messages.en
+    }
+};
+
+export const updateIntl = locale => superUpdateIntl({
+    locale: locale,
+    messages: messages[locale] || messages.en
+});

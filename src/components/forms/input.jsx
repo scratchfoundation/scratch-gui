c9565f01cabb5714ee609bbe5a8a1d157997bb88
Paul Kaplan <pkaplan@media.mit.edu>
2017-07-21 09:50:01 -0400

    Fix input prop handling

--- src/components/forms/input.jsx
+++ src/components/forms/input.jsx
@@ -4,14 +4,17 @@ import classNames from 'classnames';
 
 import styles from './input.css';
 
-const Input = props => (
-    <input
-        {...props}
-        className={classNames(styles.inputForm, {
-            [styles.inputSmall]: props.small
-        })}
-    />
-);
+const Input = props => {
+    const {small, ...componentProps} = props;
+    return (
+        <input
+            {...componentProps}
+            className={classNames(styles.inputForm, {
+                [styles.inputSmall]: small
+            })}
+        />
+    );
+};
 
 Input.propTypes = {
     small: PropTypes.bool

ecaee9877eddb2e67e84ce2016a60de74394f353
Ray Schamp <ray@scratch.mit.edu>
2017-08-15 10:30:27 -0400

    Add green flag and stop buttons to blocks example
    Add class name props to the components, and fix positioning for the green flag to be reusable.

--- src/examples/blocks-only.jsx
+++ src/examples/blocks-only.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {connect} from 'react-redux';
 
 import AppStateHOC from '../lib/app-state-hoc.jsx';
+import GreenFlag from '../containers/green-flag.jsx';
+import StopAll from '../containers/stop-all.jsx';
 import Blocks from '../containers/blocks.jsx';
 import GUI from '../containers/gui.jsx';
 import ProjectLoaderHOC from '../lib/project-loader-hoc.jsx';
@@ -10,7 +12,10 @@ import ProjectLoaderHOC from '../lib/project-loader-hoc.jsx';
 import styles from './blocks-only.css';
 
 const mapStateToProps = (state) => ({vm: state.vm});
+
 const VMBlocks = connect(mapStateToProps)(Blocks);
+const VMGreenFlag = connect(mapStateToProps)(GreenFlag);
+const VMStopAll = connect(mapStateToProps)(StopAll);
 
 const BlocksOnly = props => (
     <GUI {...props}>
@@ -20,13 +25,14 @@ const BlocksOnly = props => (
                 media: `static/blocks-media/`
             }}
         />
+        <VMGreenFlag className={styles.greenFlag} />
+        <VMStopAll className={styles.stopAll} />
     </GUI>
 );
 
 const App = AppStateHOC(ProjectLoaderHOC(BlocksOnly));
 
 const appTarget = document.createElement('div');
-appTarget.className = styles.app;
 document.body.appendChild(appTarget);
 
 ReactDOM.render(<App />, appTarget);

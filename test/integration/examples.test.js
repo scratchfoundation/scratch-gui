d54320b20ba7831de1bfda1d0ea108806c51a078
Ray Schamp <ray@scratch.mit.edu>
2017-08-17 10:30:26 -0400

    Test that blocks-only can use the blocks palette

--- test/integration/examples.test.js
+++ test/integration/examples.test.js
@@ -4,7 +4,10 @@ import path from 'path';
 import SeleniumHelper from '../helpers/selenium-helper';
 
 const {
+    clickButton,
+    clickText,
     clickXpath,
+    findByXpath,
     getDriver,
     getLogs
 } = new SeleniumHelper();
@@ -56,6 +59,28 @@ describe('blocks example', () => {
         await clickXpath('//img[@title="Go"]');
         await new Promise(resolve => setTimeout(resolve, 2000));
         await clickXpath('//img[@title="Stop"]');
+        const logs = await getLogs(errorWhitelist);
+        await expect(logs).toEqual([]);
+    });
+
+    test('Change categories', async () => {
+        await driver.get(`file://${uri}`);
+        await clickText('Looks');
+        await clickText('Sound');
+        await clickText('Pen');
+        await clickText('Events');
+        await clickText('Control');
+        await clickText('Sensing');
+        await clickText('Operators');
+        await clickText('Data');
+        await clickText('Create variable...');
+        let el = await findByXpath("//input[@placeholder='']");
+        await el.sendKeys('score');
+        await clickButton('OK');
+        await clickText('Create variable...');
+        el = await findByXpath("//input[@placeholder='']");
+        await el.sendKeys('second variable');
+        await clickButton('OK');
         const logs = await getLogs(errorWhitelist);
         await expect(logs).toEqual([]);
     });

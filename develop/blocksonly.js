13f3afb38f5ca789e801542b57c7489f361cbb05
Paul <pkaplan@media.mit.edu>
2017-09-06 20:34:45 +0000

    Build for bbfdd967e53c2afb5dd67551d7b640addc8b970c

--- develop/blocksonly.js
+++ develop/blocksonly.js
@@ -1,6 +1,6 @@
 webpackJsonp([2],{
 
-/***/ 702:
+/***/ 706:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -16,27 +16,27 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 var _reactRedux = __webpack_require__(17);
 
-var _appStateHoc = __webpack_require__(98);
+var _appStateHoc = __webpack_require__(95);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _controls = __webpack_require__(119);
+var _controls = __webpack_require__(118);
 
 var _controls2 = _interopRequireDefault(_controls);
 
-var _blocks = __webpack_require__(218);
+var _blocks = __webpack_require__(213);
 
 var _blocks2 = _interopRequireDefault(_blocks);
 
-var _gui = __webpack_require__(116);
+var _gui = __webpack_require__(115);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(122);
+var _projectLoaderHoc = __webpack_require__(121);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-var _blocksOnly = __webpack_require__(703);
+var _blocksOnly = __webpack_require__(707);
 
 var _blocksOnly2 = _interopRequireDefault(_blocksOnly);
 
@@ -72,13 +72,13 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 703:
+/***/ 707:
 /***/ (function(module, exports, __webpack_require__) {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(704);
+var content = __webpack_require__(708);
 if(typeof content === 'string') content = [[module.i, content, '']];
 // Prepare cssTransformation
 var transform;
@@ -104,7 +104,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 704:
+/***/ 708:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(5)(undefined);
@@ -121,5 +121,5 @@ exports.locals = {
 
 /***/ })
 
-},[702]);
+},[706]);
 //# sourceMappingURL=blocksonly.js.map

e772aa17cca6251c32764bd5762abfa96fd41132
Ray Schamp <ray@scratch.mit.edu>
2017-11-02 09:13:45 -0400

    Disable loader linting
    eslint-plugin-import doesn't like these inline loaders.
    
    I tried unsuccessfully to configure the recommended solution with `eslint-import-resolver-webpack`, but we should come back to this.

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -1,10 +1,13 @@
 import projectJson from './project.json';
+
+/* eslint-disable import/no-unresolved */
 import popWav from '!buffer-loader!./83a9787d4cb6f3b7632b4ddfebf74367.wav';
 import meowWav from '!buffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav';
 import backdrop from '!buffer-loader!./739b5e2a2435f6e1ec2993791b423146.png';
 import penLayer from '!buffer-loader!./5c81a336fab8be57adc039a8a2b33ca9.png';
 import costume1 from '!raw-loader!./09dc888b0b7df19f70d81588ae73420e.svg';
 import costume2 from '!raw-loader!./3696356a03a8d938318876a593572843.svg';
+/* eslint-enable import/no-unresolved */
 
 const encoder = new TextEncoder();
 export default [{

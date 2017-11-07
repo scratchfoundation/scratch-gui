c8c923b919a73671f820a1cc064b66c64cab2adc
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2017-11-07 13:45:55 -0500

    Add music to extension library

--- src/lib/libraries/extensions/index.js
+++ src/lib/libraries/extensions/index.js
@@ -1,7 +1,15 @@
+import musicImage from './music.png';
 import penImage from './pen.png';
 import wedoImage from './wedo.png';
 
 export default [
+    {
+        name: 'Music',
+        extensionURL: 'music',
+        iconURL: musicImage,
+        description: 'Play instruments and drums.',
+        featured: true
+    },
     {
         name: 'Pen',
         extensionURL: 'pen',

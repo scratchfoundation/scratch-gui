3ddb26deff8b3563922558c8a08b2273abaf3b9b
chrisgarrity <chrisg@media.mit.edu>
2018-01-10 10:13:59 -0500

    Support IE just enough to show unsupported modal (#1213)
    * Added object.assign polyfill
    
    * Fix IE load failure
    
    Don’t call `(window.AudioContext || window.webkitAudioContext)()` if platform is IE
    
    Catch any other errors, and display Browser unsupported in error-boundary.
    
    If for some reason there are no other errors (unlikely), preview-modal will still check for IE and show the unsupported modal.
    
    Polyfilling Object.assign is enough to get through load and show the modal for IE. We can always switch to `babel-polyfill` if there are others needed later, but for now this is smaller.

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import 'es6-object-assign/auto';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
--- src/lib/audio/shared-audio-context.js
+++ src/lib/audio/shared-audio-context.js
@@ -1,8 +1,12 @@
 import StartAudioContext from 'startaudiocontext';
+import platform from 'platform';
 
-const AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
+let AUDIO_CONTEXT;
+if (platform.name !== 'IE') {
+    AUDIO_CONTEXT = new (window.AudioContext || window.webkitAudioContext)();
 
-StartAudioContext(AUDIO_CONTEXT);
+    StartAudioContext(AUDIO_CONTEXT);
+}
 
 /**
  * Wrap browser AudioContext because we shouldn't create more than one

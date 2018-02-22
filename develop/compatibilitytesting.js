788f8a17ce04a2c8849575b9ac3d55807466bc1e
Paul <pkaplan@media.mit.edu>
2018-02-22 20:56:36 +0000

    Build for 3564544bc2b9443b95df05c6a996df258e2f3754

--- develop/compatibilitytesting.js
+++ develop/compatibilitytesting.js
@@ -1,6 +1,6 @@
 webpackJsonp([3],{
 
-/***/ 486:
+/***/ 487:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -147,5 +147,5 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ })
 
-},[486]);
+},[487]);
 //# sourceMappingURL=compatibilitytesting.js.map
--- develop/player.js
+++ develop/player.js
@@ -1,6 +1,6 @@
 webpackJsonp([1],{
 
-/***/ 487:
+/***/ 488:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -44,7 +44,7 @@ var _projectLoaderHoc = __webpack_require__(29);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-__webpack_require__(488);
+__webpack_require__(489);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -144,11 +144,11 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 488:
+/***/ 489:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(489);
+var content = __webpack_require__(490);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -195,7 +195,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 489:
+/***/ 490:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(2)(false);
@@ -210,5 +210,5 @@ exports.push([module.i, "body {\n    padding: 0;\n    margin: 0;\n}\n", ""]);
 
 /***/ })
 
-},[487]);
+},[488]);
 //# sourceMappingURL=player.js.map

5f1fe572064b71d58d94214a9c4576ed0f14e23f
chrisgarrity <chrisg@media.mit.edu>
2018-02-26 18:45:55 -0500

    Nicer crash message format
    fixes #1205.
    
    It doesn’t translate any of the text to avoid a case where translation is causing the error, and fails in the error boundary as well.

--- src/containers/error-boundary.jsx
+++ src/containers/error-boundary.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import platform from 'platform';
 import BrowserModalComponent from '../components/browser-modal/browser-modal.jsx';
+import CrashMessageComponent from '../components/crash-message/crash-message.jsx';
 import log from '../lib/log.js';
 import analytics from '../lib/analytics';
 
@@ -28,21 +29,16 @@ class ErrorBoundary extends React.Component {
         window.history.back();
     }
 
+    handleReload () {
+        window.location.reload();
+    }
+
     render () {
         if (this.state.hasError) {
             if (platform.name === 'IE') {
                 return <BrowserModalComponent onBack={this.handleBack} />;
             }
-            return (
-                <div style={{margin: '2rem'}}>
-                    <h1>Oops! Something went wrong.</h1>
-                    <p>
-                        We are so sorry, but it looks like Scratch has crashed. This bug has been
-                        automatically reported to the Scratch Team. Please refresh your page to try
-                        again.
-                    </p>
-                </div>
-            );
+            return <CrashMessageComponent onReload={this.handleReload} />;
         }
         return this.props.children;
     }

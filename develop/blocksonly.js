a4d10c03922313ebbb338098fc8e0c55dc1fb134
Paul <pkaplan@media.mit.edu>
2018-02-26 16:40:37 +0000

    Build for 75f086e065cdb7fac4aa4e524e6fbd8e1d3f7393

--- develop/blocksonly.js
+++ develop/blocksonly.js
@@ -1,6 +1,6 @@
 webpackJsonp([2],{
 
-/***/ 484:
+/***/ 487:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -20,23 +20,23 @@ var _appStateHoc = __webpack_require__(22);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _controls = __webpack_require__(29);
+var _controls = __webpack_require__(32);
 
 var _controls2 = _interopRequireDefault(_controls);
 
-var _blocks = __webpack_require__(87);
+var _blocks = __webpack_require__(93);
 
 var _blocks2 = _interopRequireDefault(_blocks);
 
-var _gui = __webpack_require__(25);
+var _gui = __webpack_require__(26);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(30);
+var _projectLoaderHoc = __webpack_require__(33);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-var _blocksOnly = __webpack_require__(485);
+var _blocksOnly = __webpack_require__(488);
 
 var _blocksOnly2 = _interopRequireDefault(_blocksOnly);
 
@@ -72,11 +72,11 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 485:
+/***/ 488:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(486);
+var content = __webpack_require__(489);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -123,7 +123,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 486:
+/***/ 489:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(2)(false);
@@ -140,5 +140,5 @@ exports.locals = {
 
 /***/ })
 
-},[484]);
+},[487]);
 //# sourceMappingURL=blocksonly.js.map

302babdeeebb9a151727f611edcd6f354aebc046
Paul <pkaplan@media.mit.edu>
2018-02-26 16:12:24 +0000

    Build for e56d69ccc3f6ce50c30e0e211baeec3430505fe9

--- asset-menus/compatibilitytesting.js
+++ asset-menus/compatibilitytesting.js
@@ -1,6 +1,6 @@
 webpackJsonp([3],{
 
-/***/ 483:
+/***/ 490:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24,11 +24,11 @@ var _appStateHoc = __webpack_require__(22);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _controls = __webpack_require__(31);
+var _controls = __webpack_require__(32);
 
 var _controls2 = _interopRequireDefault(_controls);
 
-var _stage = __webpack_require__(54);
+var _stage = __webpack_require__(52);
 
 var _stage2 = _interopRequireDefault(_stage);
 
@@ -36,11 +36,11 @@ var _box = __webpack_require__(7);
 
 var _box2 = _interopRequireDefault(_box);
 
-var _gui = __webpack_require__(25);
+var _gui = __webpack_require__(26);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(32);
+var _projectLoaderHoc = __webpack_require__(33);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
@@ -147,5 +147,5 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ })
 
-},[483]);
+},[490]);
 //# sourceMappingURL=compatibilitytesting.js.map
--- asset-menus/player.js
+++ asset-menus/player.js
@@ -1,6 +1,6 @@
 webpackJsonp([1],{
 
-/***/ 484:
+/***/ 491:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -24,11 +24,11 @@ var _appStateHoc = __webpack_require__(22);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _controls = __webpack_require__(31);
+var _controls = __webpack_require__(32);
 
 var _controls2 = _interopRequireDefault(_controls);
 
-var _stage = __webpack_require__(54);
+var _stage = __webpack_require__(52);
 
 var _stage2 = _interopRequireDefault(_stage);
 
@@ -36,15 +36,15 @@ var _box = __webpack_require__(7);
 
 var _box2 = _interopRequireDefault(_box);
 
-var _gui = __webpack_require__(25);
+var _gui = __webpack_require__(26);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(32);
+var _projectLoaderHoc = __webpack_require__(33);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-__webpack_require__(485);
+__webpack_require__(492);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -144,11 +144,11 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 485:
+/***/ 492:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(486);
+var content = __webpack_require__(493);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -195,7 +195,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 486:
+/***/ 493:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(2)(false);
@@ -210,5 +210,5 @@ exports.push([module.i, "body {\n    padding: 0;\n    margin: 0;\n}\n", ""]);
 
 /***/ })
 
-},[484]);
+},[491]);
 //# sourceMappingURL=player.js.map

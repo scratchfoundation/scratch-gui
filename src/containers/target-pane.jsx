76c0155bc55b8777794a781e9f2f3e32833f8874
DD <liudi08@gmail.com>
2018-03-08 16:37:19 -0500

    Clean up some unused warnings

--- src/containers/target-pane.jsx
+++ src/containers/target-pane.jsx
@@ -87,6 +87,7 @@ class TargetPane extends React.Component {
     render () {
         const {
             onActivateTab, // eslint-disable-line no-unused-vars
+            onReceivedBlocks, // eslint-disable-line no-unused-vars
             ...componentProps
         } = this.props;
         return (

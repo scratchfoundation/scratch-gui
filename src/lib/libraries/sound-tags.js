32b10344cf1ec3067b85436a8174b049d864606b
Ray Schamp <ray@scratch.mit.edu>
2018-04-19 13:54:35 -0400

    Add sound tags
    Thanks @ericrosenbaum!

--- src/lib/libraries/sound-tags.js
+++ src/lib/libraries/sound-tags.js
@@ -1,3 +1,11 @@
 export default [
-    {title: 'Loops'}
+    {title: 'Animal'},
+    {title: 'Effects'},
+    {title: 'Loops'},
+    {title: 'Notes'},
+    {title: 'Percussion'},
+    {title: 'Space'},
+    {title: 'Sports'},
+    {title: 'Voice'},
+    {title: 'Wacky'}
 ];

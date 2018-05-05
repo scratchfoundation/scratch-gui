317f11ed92575c63fbc64040bcf7b20edc1324ce
Ray Schamp <ray@scratch.mit.edu>
2018-05-04 23:28:55 -0400

    Use bowser rather than platform
    bowser correctly detects Chrome Headless as Chrome, is already used by scratchr2, and is more actively maintained than platform.

--- src/lib/supported-browser.js
+++ src/lib/supported-browser.js
@@ -1,14 +1,14 @@
-import platform from 'platform';
+import bowser from 'bowser';
 
 /**
  * Helper function to determine if the browser is supported.
  * @returns {boolean} False if the platform is definitely not supported.
  */
 export default function () {
-    if (platform.name === 'IE' ||
-        platform.name === 'Opera' ||
-        platform.name === 'Opera Mini' ||
-        platform.name === 'Silk') {
+    if (bowser.name === 'IE' ||
+        bowser.name === 'Opera' ||
+        bowser.name === 'Opera Mini' ||
+        bowser.name === 'Silk') {
         return false;
     }
     // @todo Should also test for versions of supported browsers

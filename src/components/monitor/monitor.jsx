96d6391bb3236a8a1f48f7386391d82cea94c217
Paul Kaplan <pkaplan@media.mit.edu>
2018-05-09 12:51:52 -0400

    Add "read-only" slider and list monitors.
    Includes:
    - context menu for switching to sliders, only on variables.
    - width, height, min, max properties consumed from VM.

--- src/components/monitor/monitor.jsx
+++ src/components/monitor/monitor.jsx
@@ -7,6 +7,8 @@ import {ContextMenu, MenuItem} from '../context-menu/context-menu.jsx';
 import Box from '../box/box.jsx';
 import DefaultMonitor from './default-monitor.jsx';
 import LargeMonitor from './large-monitor.jsx';
+import SliderMonitor from './slider-monitor.jsx';
+import ListMonitor from './list-monitor.jsx';
 
 import styles from './monitor.css';
 
@@ -21,44 +23,62 @@ const categories = {
 
 const modes = {
     default: DefaultMonitor,
-    large: LargeMonitor
+    large: LargeMonitor,
+    slider: SliderMonitor,
+    list: ListMonitor
 };
 
 const MonitorComponent = props => (
     <ContextMenuTrigger id={`monitor-${props.label}`}>
         <Draggable
             bounds=".monitor-overlay" // Class for monitor container
+            cancel=".no-drag" // Class used for slider input to prevent drag
             defaultClassNameDragging={styles.dragging}
             onStop={props.onDragEnd}
         >
             <Box
                 className={styles.monitorContainer}
                 componentRef={props.componentRef}
-                onDoubleClick={props.onNextMode}
+                onDoubleClick={props.mode === 'list' ? null : props.onNextMode}
             >
                 {(modes[props.mode] || modes.default)({ // Use default until other modes arrive
                     categoryColor: categories[props.category],
                     label: props.label,
-                    value: props.value
+                    value: props.value,
+                    width: props.width,
+                    height: props.height,
+                    min: props.min,
+                    max: props.max
                 })}
             </Box>
         </Draggable>
-        <ContextMenu id={`monitor-${props.label}`}>
-            <MenuItem onClick={props.onSetModeToDefault}>
-                <FormattedMessage
-                    defaultMessage="normal readout"
-                    description="Menu item to switch to the default monitor"
-                    id="gui.monitor.contextMenu.default"
-                />
-            </MenuItem>
-            <MenuItem onClick={props.onSetModeToLarge}>
-                <FormattedMessage
-                    defaultMessage="large readout"
-                    description="Menu item to switch to the large monitor"
-                    id="gui.monitor.contextMenu.large"
-                />
-            </MenuItem>
-        </ContextMenu>
+        {props.mode === 'list' ? null : (
+            <ContextMenu id={`monitor-${props.label}`}>
+                <MenuItem onClick={props.onSetModeToDefault}>
+                    <FormattedMessage
+                        defaultMessage="normal readout"
+                        description="Menu item to switch to the default monitor"
+                        id="gui.monitor.contextMenu.default"
+                    />
+                </MenuItem>
+                <MenuItem onClick={props.onSetModeToLarge}>
+                    <FormattedMessage
+                        defaultMessage="large readout"
+                        description="Menu item to switch to the large monitor"
+                        id="gui.monitor.contextMenu.large"
+                    />
+                </MenuItem>
+                {props.onSetModeToSlider ? (
+                    <MenuItem onClick={props.onSetModeToSlider}>
+                        <FormattedMessage
+                            defaultMessage="slider"
+                            description="Menu item to switch to the slider monitor"
+                            id="gui.monitor.contextMenu.slider"
+                        />
+                    </MenuItem>
+                ) : null}
+            </ContextMenu>
+        )}
     </ContextMenuTrigger>
 
 );
@@ -70,15 +90,25 @@ const monitorModes = Object.keys(modes);
 MonitorComponent.propTypes = {
     category: PropTypes.oneOf(Object.keys(categories)),
     componentRef: PropTypes.func.isRequired,
+    height: PropTypes.number,
     label: PropTypes.string.isRequired,
+    max: PropTypes.number,
+    min: PropTypes.number,
     mode: PropTypes.oneOf(monitorModes),
     onDragEnd: PropTypes.func.isRequired,
     onNextMode: PropTypes.func.isRequired,
     onSetModeToDefault: PropTypes.func.isRequired,
     onSetModeToLarge: PropTypes.func.isRequired,
+    onSetModeToSlider: PropTypes.func,
     value: PropTypes.oneOfType([
         PropTypes.string,
-        PropTypes.number])
+        PropTypes.number,
+        PropTypes.arrayOf(PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.number
+        ]))
+    ]),
+    width: PropTypes.number
 };
 
 MonitorComponent.defaultProps = {
--- src/components/monitor/slider-monitor.jsx
+++ src/components/monitor/slider-monitor.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './monitor.css';
 
-const DefaultMonitor = ({categoryColor, label, value}) => (
+const SliderMonitor = ({categoryColor, label, min, max, value}) => (
     <div className={styles.defaultMonitor}>
         <div className={styles.row}>
             <div className={styles.label}>
@@ -15,16 +15,35 @@ const DefaultMonitor = ({categoryColor, label, value}) => (
                 {value}
             </div>
         </div>
+        <div className={styles.row}>
+            <input
+                className="no-drag" // Class used on parent Draggable to prevent drags
+                max={max}
+                min={min}
+                type="range"
+                value={value}
+                // @todo onChange callback
+            />
+        </div>
+
     </div>
 );
 
-DefaultMonitor.propTypes = {
+SliderMonitor.propTypes = {
     categoryColor: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
+    max: PropTypes.number,
+    min: PropTypes.number,
+    // @todo callback for change events
     value: PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.number
     ])
 };
 
-export default DefaultMonitor;
+SliderMonitor.defaultProps = {
+    min: 0,
+    max: 100
+};
+
+export default SliderMonitor;

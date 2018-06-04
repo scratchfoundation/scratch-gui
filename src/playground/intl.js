6834449a856a14efc5a82f0fab52d10a6e6da243
chrisgarrity <chrisg@media.mit.edu>
2018-06-04 15:59:25 -0400

    Update intl for l10n 3+

--- src/playground/intl.js
+++ src/playground/intl.js
@@ -1,25 +1,19 @@
 import {addLocaleData} from 'react-intl';
-import defaultsDeep from 'lodash.defaultsdeep';
 
 import localeData from 'scratch-l10n';
-import guiMessages from 'scratch-l10n/locales/gui-msgs';
-import paintMessages from 'scratch-l10n/locales/paint-msgs';
-import penMessages from 'scratch-l10n/locales/pen-msgs';
-
-const combinedMessages = defaultsDeep({}, guiMessages.messages, paintMessages.messages, penMessages.messages);
+import editorMessages from 'scratch-l10n/locales/editor-msgs';
 
 Object.keys(localeData).forEach(locale => {
-    // TODO: will need to handle locales not in the default intl - see www/custom-locales
     addLocaleData(localeData[locale].localeData);
 });
 
 const intlDefault = {
     defaultLocale: 'en',
     locale: 'en',
-    messages: combinedMessages.en.messages
+    messages: editorMessages.en
 };
 
 export {
     intlDefault as default,
-    combinedMessages
+    editorMessages
 };

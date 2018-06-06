12b145892ac30857b645c2f714be2396d33982a0
Paul <pkaplan@media.mit.edu>
2018-06-06 23:16:12 +0000

    Build for b4d5792fa8981ef70d9218bb33ea5c46ccfa5a55

--- develop/gui.js
+++ develop/gui.js
@@ -15,11 +15,11 @@ var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = __webpack_require__(32);
+var _reactDom = __webpack_require__(33);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _analytics = __webpack_require__(34);
+var _analytics = __webpack_require__(32);
 
 var _analytics2 = _interopRequireDefault(_analytics);
 

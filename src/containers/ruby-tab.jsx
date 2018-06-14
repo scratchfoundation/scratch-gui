fca278dd31e542ec7f9f9012291beb7d58964b16
t-kazu <s153057@matsu.shimane-u.ac.jp>
2018-06-14 21:30:23 +0900

    ruby-editor用のオプションの付加しました

--- src/containers/ruby-tab.jsx
+++ src/containers/ruby-tab.jsx
@@ -3,16 +3,24 @@ import AceEditor from 'react-ace';
 import brace from 'brace';
 
 import 'brace/mode/ruby';
-import 'brace/theme/github';
+import 'brace/theme/clouds';
 
 export class RubyTab extends React.Component {
     render(){
         return(
             <AceEditor
                 mode="ruby"
-                theme="github"
-                name="UNIQUE_ID_OF_DIV"
-                editorProps={{$blockScrolling: true}}
+                theme="clouds"
+                name="ruby-editor"
+                fontSize={16}
+                width="100%"
+                height="inherit"
+                editorProps={{ $blockScrolling: true }}
+                setOptions={{
+                  tabSize: 2,
+                  useSoftTabs: true,
+                  showInvisibles: true
+                }}
             />
         )
     }

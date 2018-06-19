9ce75e805a4a95f007c9be482b9eb7c9262a8b59
Paul Kaplan <pkaplan@media.mit.edu>
2018-06-19 08:31:02 -0400

    Update backpack api to use `xhr` module

--- src/lib/backpack-api.js
+++ src/lib/backpack-api.js
@@ -1,18 +1,28 @@
+import xhr from 'xhr';
+
 const getBackpackContents = ({
     host,
     username,
     token,
     limit,
     offset
-}) => fetch(`${host}${username}?limit=${limit}&offset=${offset}`, {
-    headers: {'x-token': token}
-})
-    .then(d => d.json())
-    .then(items => items.map(item =>
+}) => new Promise((resolve, reject) => {
+    xhr({
+        method: 'GET',
+        uri: `${host}${username}?limit=${limit}&offset=${offset}`,
+        headers: {'x-token': token},
+        json: true
+    }, (error, response) => {
+        if (error || response.statusCode !== 200) {
+            return reject();
+        }
         // Add a new property for the full thumbnail url, which includes the host.
         // TODO retreiving the images through storage would allow us to remove this.
-        Object.assign(item, {thumbnailUrl: `${host}${item.thumbnail}`})
-    ));
+        return resolve(response.body.map(item => (
+            Object.assign({}, item, {thumbnailUrl: `${host}/${item.thumbnail}`})
+        )));
+    });
+});
 
 export {
     getBackpackContents

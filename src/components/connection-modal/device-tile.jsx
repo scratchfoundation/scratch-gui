d34746f29bcd94e9fcdeb22d34502d7644e0a19d
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2018-06-26 21:18:10 -0400

    Make device tile into a class component

--- src/components/connection-modal/device-tile.jsx
+++ src/components/connection-modal/device-tile.jsx
@@ -1,36 +1,48 @@
 import {FormattedMessage} from 'react-intl';
 import PropTypes from 'prop-types';
 import React from 'react';
-
+import bindAll from 'lodash.bindall';
 import Box from '../box/box.jsx';
 
 import styles from './connection-modal.css';
 
-//@todo make this into a 'class component'
-const DeviceTile = props => (
-    <Box className={styles.deviceTile}>
-        <Box>
-            <span>{props.name}</span>
-            <Box className={styles.deviceTileWidgets}>
-                <span className={styles.signalStrengthText}>{props.RSSI}</span>
-                <button
-                    onClick={()=>props.onConnecting(props.peripheralId)}
-                >
-                    <FormattedMessage
-                        defaultMessage="connect"
-                        description=""
-                        id="gui.connection.connect"
-                    />
-                </button>
+class DeviceTile extends React.Component {
+    constructor (props) {
+        super(props);
+        bindAll(this, [
+            'handleConnecting'
+        ]);
+    }
+    handleConnecting () {
+        this.props.onConnecting(this.props.peripheralId);
+    }
+    render () {
+        return (
+            <Box className={styles.deviceTile}>
+                <Box>
+                    <span>{this.props.name}</span>
+                    <Box className={styles.deviceTileWidgets}>
+                        <span className={styles.signalStrengthText}>{this.props.RSSI}</span>
+                        <button
+                            onClick={this.handleConnecting}
+                        >
+                            <FormattedMessage
+                                defaultMessage="connect"
+                                description=""
+                                id="gui.connection.connect"
+                            />
+                        </button>
+                    </Box>
+                </Box>
             </Box>
-        </Box>
-    </Box>
-);
+        );
+    }
+}
 
 DeviceTile.propTypes = {
     RSSI: PropTypes.number,
     name: PropTypes.string,
-    onConnecting: PropTypes.function,
+    onConnecting: PropTypes.func,
     peripheralId: PropTypes.string
 };
 

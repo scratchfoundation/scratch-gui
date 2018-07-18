248e4c3e9db4cf6e1a48f7237309d954e6fcede2
Andrew <andrewsliwinski@acm.org>
2018-07-18 14:52:04 +0000

    Build for cd145c7b29c6312120d5a6c7b8906edb4aa6080d

--- develop/blocksonly.js
+++ develop/blocksonly.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[2],{
 
-/***/ 1480:
+/***/ 1478:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -37,7 +37,7 @@ var _appStateHoc = __webpack_require__(165);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _blocksOnly = __webpack_require__(1481);
+var _blocksOnly = __webpack_require__(1479);
 
 var _blocksOnly2 = _interopRequireDefault(_blocksOnly);
 
@@ -73,11 +73,11 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 1481:
+/***/ 1479:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(1482);
+var content = __webpack_require__(1480);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -99,7 +99,7 @@ if(false) {}
 
 /***/ }),
 
-/***/ 1482:
+/***/ 1480:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(3)(false);
@@ -116,5 +116,5 @@ exports.locals = {
 
 /***/ })
 
-},[[1480,0]]]);
+},[[1478,0]]]);
 //# sourceMappingURL=blocksonly.js.map
--- develop/player.js
+++ develop/player.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[4],{
 
-/***/ 1484:
+/***/ 1482:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -45,7 +45,7 @@ var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
 var _mode = __webpack_require__(118);
 
-var _player = __webpack_require__(1485);
+var _player = __webpack_require__(1483);
 
 var _player2 = _interopRequireDefault(_player);
 
@@ -109,11 +109,11 @@ _reactDom2.default.render(_react2.default.createElement(WrappedPlayer, { isPlaye
 
 /***/ }),
 
-/***/ 1485:
+/***/ 1483:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(1486);
+var content = __webpack_require__(1484);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -135,7 +135,7 @@ if(false) {}
 
 /***/ }),
 
-/***/ 1486:
+/***/ 1484:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(3)(false);
@@ -153,5 +153,5 @@ exports.locals = {
 
 /***/ })
 
-},[[1484,0]]]);
+},[[1482,0]]]);
 //# sourceMappingURL=player.js.map

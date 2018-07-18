64a13ffb9b4f6cec8bc7d79f544abab8bb7b4feb
Paul <pkaplan@media.mit.edu>
2018-07-18 18:13:55 +0000

    Build for 0d1e6871523a526443dace7f75003e116c159a69

--- develop/compatibilitytesting.js
+++ develop/compatibilitytesting.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[3],{
 
-/***/ 1485:
+/***/ 1487:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -107,5 +107,5 @@ _reactDom2.default.render(_react2.default.createElement(Player, null), appTarget
 
 /***/ })
 
-},[[1485,0]]]);
+},[[1487,0]]]);
 //# sourceMappingURL=compatibilitytesting.js.map

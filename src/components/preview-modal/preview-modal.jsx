f51326915906fa016d322c272aea331eeceed974
apple502j <33279053+apple502j@users.noreply.github.com>
2018-08-01 18:06:44 +0900

    lint

--- src/components/preview-modal/preview-modal.jsx
+++ src/components/preview-modal/preview-modal.jsx
@@ -28,40 +28,48 @@ const PreviewModal = ({intl, ...props}) => (
         <Box className={styles.body}>
             <h2>
                 <FormattedMessage
-                    defaultMessage="Welcome to the Scratch 3.0 Preview"
-                    description="Header for Preview Info Modal"
-                    id="gui.previewInfo.welcome"
+                    defaultMessage='Welcome to the Scratch 3.0 Preview'
+                    description='Header for Preview Info Modal'
+                    id='gui.previewInfo.welcome'
                 />
             </h2>
             <p>
                 <FormattedMessage
-                    defaultMessage="We're working on the next generation of Scratch. We're excited for you to try it!"
-                    description="Invitation to try 3.0 preview"
-                    id="gui.previewInfo.invitation"
+                    defaultMessage='We're working on the next generation of Scratch. We're excited for you to try it!'
+                    description='Invitation to try 3.0 preview'
+                    id='gui.previewInfo.invitation'
                 />
             </p>
 
             <Box className={styles.buttonRow}>
                 <button
                     className={styles.noButton}
+                    title={intl.formatMessage({
+                        defaultMessage: 'Not Now',
+                        description: 'Tooltip for Not Now button',
+                        id: 'gui.previewModal.notnowtooltip'
+                    })}
                     onClick={props.onCancel}
-                    title={intl.formatMessage({defaultMessage: "Not Now", description: "Tooltip for Not Now button", id: "gui.previewModal.notnowtooltip"})}
                 >
                     <FormattedMessage
-                        defaultMessage="Not Now"
-                        description="Label for button to back out of trying Scratch 3.0 preview"
-                        id="gui.previewInfo.notnow"
+                        defaultMessage='Not Now'
+                        description='Label for button to back out of trying Scratch 3.0 preview'
+                        id='gui.previewInfo.notnow'
                     />
                 </button>
                 <button
                     className={styles.okButton}
-                    title={intl.formatMessage({defaultMessage: "Try It", description: "Tooltip for Try It button", id: "gui.previewModal.tryittooltip"})}
+                    title={intl.formatMessage({
+                        defaultMessage: 'Try It',
+                        description: 'Tooltip for Try It button',
+                        id: 'gui.previewModal.tryittooltip'
+                    })}
                     onClick={props.onTryIt}
                 >
                     <FormattedMessage
-                        defaultMessage="Try It! {caticon}"
-                        description="Label for button to try Scratch 3.0 preview"
-                        id="gui.previewModal.tryit"
+                        defaultMessage='Try It! {caticon}'
+                        description='Label for button to try Scratch 3.0 preview'
+                        id='gui.previewModal.tryit'
                         values={{
                             caticon: (
                                 <img
@@ -74,31 +82,35 @@ const PreviewModal = ({intl, ...props}) => (
                 </button>
                 <button
                     className={styles.viewProjectButton}
-                    title={intl.formatMessage({defaultMessage: "View 2.0 Project", description: "Tooltip for View 2.0 Project button", id: "gui.previewModal.viewprojecttooltip"})}
+                    title={intl.formatMessage({
+                        defaultMessage: 'View 2.0 Project',
+                        description: 'Tooltip for View 2.0 Project button',
+                        id: 'gui.previewModal.viewprojecttooltip'
+                    })}
                     onClick={props.onViewProject}
                 >
                     <FormattedMessage
-                        defaultMessage="View 2.0 Project"
-                        description="Label for button to import a 2.0 project"
-                        id="gui.previewModal.viewproject"
+                        defaultMessage='View 2.0 Project'
+                        description='Label for button to import a 2.0 project'
+                        id='gui.previewModal.viewproject'
                     />
                 </button>
             </Box>
             <Box className={styles.faqLinkText}>
                 <FormattedMessage
-                    defaultMessage="To learn more, go to the {previewFaqLink}."
-                    description="Invitation to try 3.0 preview"
-                    id="gui.previewInfo.previewfaq"
+                    defaultMessage='To learn more, go to the {previewFaqLink}.'
+                    description='Invitation to try 3.0 preview'
+                    id='gui.previewInfo.previewfaq'
                     values={{
                         previewFaqLink: (
                             <a
                                 className={styles.faqLink}
-                                href="//scratch.mit.edu/preview-faq"
+                                href='//scratch.mit.edu/preview-faq'
                             >
                                 <FormattedMessage
-                                    defaultMessage="Preview FAQ"
-                                    description="link to Scratch 3.0 preview FAQ page"
-                                    id="gui.previewInfo.previewfaqlink"
+                                    defaultMessage='Preview FAQ'
+                                    description='link to Scratch 3.0 preview FAQ page'
+                                    id='gui.previewInfo.previewfaqlink'
                                 />
                             </a>
                         )

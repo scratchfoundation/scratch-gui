72616cdabfbde73cde847e04c20fbfb1f33f6e6c
chrisgarrity <chrisg@media.mit.edu>
2018-09-16 17:43:51 -0400

    Localize default names for new blank objects
    Questions:
    * should the message strings include the number? Or should that get added to the name when the name is defined
    * added a new ‘shared-messages’ to handle the definition of backdrop, costume, etc which are used in multiple places, they’re all related to defaults, so should it all be in `lib/default-project`

---
+++ src/lib/shared-messages.js
@@ -0,0 +1,24 @@
+import {defineMessages} from 'react-intl';
+
+export default defineMessages({
+    backdrop: {
+        defaultMessage: 'backdrop1',
+        description: 'Default name for a new backdrop, scratch will automatically adjust the number if necessary',
+        id: 'gui.sharedMessages.backdrop'
+    },
+    costume: {
+        defaultMessage: 'costume1',
+        description: 'Default name for a new costume, scratch will automatically adjust the number if necessary',
+        id: 'gui.sharedMessages.costume'
+    },
+    sprite: {
+        defaultMessage: 'sprite1',
+        description: 'Default name for a new sprite, scratch will automatically adjust the number if necessary',
+        id: 'gui.sharedMessages.sprite'
+    },
+    sound: {
+        defaultMessage: 'pop',
+        description: 'Name of the pop sound, the default sound added to a sprite',
+        id: 'gui.sharedMessages.pop'
+    }
+});

4571147da304a9d38f21051330b782982b7aff77
DD <liudi08@gmail.com>
2018-09-26 21:15:58 +0000

    Build for 6bfef4e3eb17c77971cebb13cbf9f5893aae77a4

--- greenkeeper/scratch-paint-0.2.0-prerelease.20180926191006/compatibilitytesting.js
+++ greenkeeper/scratch-paint-0.2.0-prerelease.20180926191006/compatibilitytesting.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[3],{
 
-/***/ 1562:
+/***/ 1556:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -25,7 +25,7 @@ var _hashParserHoc = __webpack_require__(183);
 
 var _hashParserHoc2 = _interopRequireDefault(_hashParserHoc);
 
-var _appStateHoc = __webpack_require__(113);
+var _appStateHoc = __webpack_require__(114);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
@@ -107,5 +107,5 @@ _reactDom2.default.render(_react2.default.createElement(Player, null), appTarget
 
 /***/ })
 
-},[[1562,0]]]);
+},[[1556,0]]]);
 //# sourceMappingURL=compatibilitytesting.js.map
--- greenkeeper/scratch-paint-0.2.0-prerelease.20180926191006/player.js
+++ greenkeeper/scratch-paint-0.2.0-prerelease.20180926191006/player.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[4],{
 
-/***/ 1563:
+/***/ 1557:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -39,17 +39,17 @@ var _hashParserHoc = __webpack_require__(183);
 
 var _hashParserHoc2 = _interopRequireDefault(_hashParserHoc);
 
-var _appStateHoc = __webpack_require__(113);
+var _appStateHoc = __webpack_require__(114);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _titledHoc = __webpack_require__(462);
+var _titledHoc = __webpack_require__(458);
 
 var _titledHoc2 = _interopRequireDefault(_titledHoc);
 
-var _mode = __webpack_require__(114);
+var _mode = __webpack_require__(115);
 
-var _player = __webpack_require__(1564);
+var _player = __webpack_require__(1558);
 
 var _player2 = _interopRequireDefault(_player);
 
@@ -116,11 +116,11 @@ _reactDom2.default.render(_react2.default.createElement(WrappedPlayer, { isPlaye
 
 /***/ }),
 
-/***/ 1564:
+/***/ 1558:
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(1565);
+var content = __webpack_require__(1559);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -142,7 +142,7 @@ if(false) {}
 
 /***/ }),
 
-/***/ 1565:
+/***/ 1559:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(4)(false);
@@ -160,5 +160,5 @@ exports.locals = {
 
 /***/ })
 
-},[[1563,0]]]);
+},[[1557,0]]]);
 //# sourceMappingURL=player.js.map

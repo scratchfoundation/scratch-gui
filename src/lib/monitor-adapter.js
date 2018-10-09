68b3629ff1c2f5c391aca834f51a8fafda395a42
chrisgarrity <chrisg@media.mit.edu>
2018-10-09 18:19:12 -0400

    Update monitor labels on the stage when language changes
    A complete rewrite of OpcodeLabels. Instead of function, it’s a class that maintains a current translator function, and updates the opcode labelFn for each opcode when a new translator function is set. Add function `getLabel` to return the object with `category` and `labelFn` based on the opcode.
    
    Set intl.formatMessage as the translator function in MonitorList. It has access to the intl object, and wraps all monitors.

--- src/lib/monitor-adapter.js
+++ src/lib/monitor-adapter.js
@@ -17,7 +17,7 @@ const isUndefined = a => typeof a === 'undefined';
 export default function ({id, spriteName, opcode, params, value, vm}) {
     // Extension monitors get their labels from the Runtime through `getLabelForOpcode`.
     // Other monitors' labels are hard-coded in `OpcodeLabels`.
-    let {label, category, labelFn} = (vm && vm.runtime.getLabelForOpcode(opcode)) || OpcodeLabels(opcode);
+    let {label, category, labelFn} = (vm && vm.runtime.getLabelForOpcode(opcode)) || OpcodeLabels.getLabel(opcode);
 
     // Use labelFn if provided for dynamic labelling (e.g. variables)
     if (!isUndefined(labelFn)) label = labelFn(params);

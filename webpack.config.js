9b7cc47c45363574659b8a4f51c53fed55d9096c
Ray Schamp <ray@scratch.mit.edu>
2018-10-11 15:37:47 +0100

    Remove unused environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -129,7 +129,6 @@ module.exports = [
         },
         plugins: base.plugins.concat([
             new webpack.DefinePlugin({
-                'process.env.API_HOST': '"' + process.env.API_HOST + '"',
                 'process.env.NODE_ENV': '"' + process.env.NODE_ENV + '"',
                 'process.env.DEBUG': Boolean(process.env.DEBUG),
                 'process.env.GA_ID': '"' + (process.env.GA_ID || 'UA-000000-01') + '"'

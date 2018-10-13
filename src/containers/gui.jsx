e7f1dce72805809ffeeb83ae2ff26cc3b250efde
DD Liu <liudi@media.mit.edu>
2018-10-12 20:43:31 -0400

    Fix for mac chrome"

--- src/containers/gui.jsx
+++ src/containers/gui.jsx
@@ -48,7 +48,7 @@ class GUI extends React.Component {
         this.props.vm.attachAudioEngine(this.audioEngine);
         this.props.vm.initialized = true;
         const fontPromises = [];
-        if (document.fonts && document.fonts.values.length) {
+        if (document.fonts && document.fonts.values() && document.fonts.values().next) {
             for (const fontFace of document.fonts.values()) {
                 fontPromises.push(fontFace.loaded);
                 fontFace.load();

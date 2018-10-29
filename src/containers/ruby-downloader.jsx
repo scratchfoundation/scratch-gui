2eea0c4d5788d5fe689c63808fa46fc975928820
t-kazu <s153057@matsu.shimane-u.ac.jp>
2018-10-29 11:50:55 +0900

    rbファイルでダウンロードできるようにしました

--- src/containers/ruby-downloader.jsx
+++ src/containers/ruby-downloader.jsx
@@ -2,7 +2,6 @@ import bindAll from 'lodash.bindall';
 import PropTypes from 'prop-types';
 import React from 'react';
 import {connect} from 'react-redux';
-import JSZip from 'jszip';
 import {projectTitleInitialState} from '../reducers/project-title';
 
 class RubyDownloader extends React.Component {
@@ -13,43 +12,29 @@ class RubyDownloader extends React.Component {
         ]);
     }
     saveRuby () {
-        const projectRuby = `require "smalruby"\n${this.props.rubyCode}`;
-        // TODO want to eventually move zip creation out of here, and perhaps
-        // into scratch-storage
-        const zip = new JSZip();
-
-        // Put everything in a zip file
-        zip.file('project.rb', projectRuby);
-
-        return zip.generateAsync({
-            type: 'blob',
-            compression: 'DEFLATE',
-            compressionOptions: {
-                level: 6 // Tradeoff between best speed (1) and best compression (9)
-            }
-        });
+        const projectRuby = `require "smalruby3"\n\n${this.props.rubyCode}`;
+        return new Blob([projectRuby], {type: 'text/plain'});
     }
     downloadProject () {
         const downloadLink = document.createElement('a');
         document.body.appendChild(downloadLink);
 
-        this.saveRuby().then(content => {
-            if (this.props.onSaveFinished) {
-                this.props.onSaveFinished();
-            }
-            // Use special ms version if available to get it working on Edge.
-            if (navigator.msSaveOrOpenBlob) {
-                navigator.msSaveOrOpenBlob(content, this.props.projectFilename);
-                return;
-            }
+        const content = this.saveRuby();
+        if (this.props.onSaveFinished) {
+            this.props.onSaveFinished();
+        }
+        // Use special ms version if available to get it working on Edge.
+        if (navigator.msSaveOrOpenBlob) {
+            navigator.msSaveOrOpenBlob(content, this.props.projectFilename);
+            return;
+        }
 
-            const url = window.URL.createObjectURL(content);
-            downloadLink.href = url;
-            downloadLink.download = this.props.projectFilename;
-            downloadLink.click();
-            window.URL.revokeObjectURL(url);
-            document.body.removeChild(downloadLink);
-        });
+        const url = window.URL.createObjectURL(content);
+        downloadLink.href = url;
+        downloadLink.download = this.props.projectFilename;
+        downloadLink.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(downloadLink);
     }
     render () {
         const {
@@ -66,7 +51,7 @@ const getProjectFilename = (curTitle, defaultTitle) => {
     if (!filenameTitle || filenameTitle.length === 0) {
         filenameTitle = defaultTitle;
     }
-    return `${filenameTitle.substring(0, 100)}.zip`;
+    return `${filenameTitle.substring(0, 100)}.rb`;
 };
 
 RubyDownloader.propTypes = {

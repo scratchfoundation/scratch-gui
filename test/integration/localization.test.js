130bd3f05cd737b2d4f782efb067c033cb3aa092
Paul Kaplan <pkaplan@media.mit.edu>
2018-10-30 14:05:30 -0400

    Add integration test for changing languages not clearing project

--- test/integration/localization.test.js
+++ test/integration/localization.test.js
@@ -6,7 +6,9 @@ const {
     clickXpath,
     getDriver,
     getLogs,
-    loadUri
+    loadUri,
+    scope,
+    rightClickText
 } = new SeleniumHelper();
 
 const uri = path.resolve(__dirname, '../../build/index.html');
@@ -25,6 +27,13 @@ describe('Localization', () => {
     test('Localization', async () => {
         await loadUri(uri);
         await clickXpath('//button[@title="Try It"]');
+
+        // Add a sprite to make sure it stays when switching languages
+        await clickText('Costumes');
+        await clickXpath('//button[@aria-label="Choose a Sprite"]');
+        await clickText('Apple', scope.modal); // Closes modal
+
+        await clickText('Code');
         await clickXpath('//*[@aria-label="language selector"]');
         await clickText('Deutsch');
         await new Promise(resolve => setTimeout(resolve, 1000)); // wait for blocks refresh
@@ -36,6 +45,9 @@ describe('Localization', () => {
 
         // Change to the costumes tab to confirm other parts of the GUI are translating
         await clickText('Kostüme');
+
+        // After switching languages, make sure Apple sprite still exists
+        await rightClickText('Apple', scope.spriteTile); // Make sure it is there
 
         const logs = await getLogs();
         await expect(logs).toEqual([]);

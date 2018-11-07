99536109be8338d7693f36900be8234843f8e4d1
Paul Kaplan <pkaplan@media.mit.edu>
2018-11-07 09:38:49 -0500

    Use bind instead of function => function => to clarify

--- src/lib/backpack-api.js
+++ src/lib/backpack-api.js
@@ -75,7 +75,7 @@ const deleteBackpackObject = ({
 
 // Two types of backpack items are not retreivable through storage
 // code, as json and sprite3 as arraybuffer zips.
-const fetchAs = responseType => uri => new Promise((resolve, reject) => {
+const fetchAs = (responseType, uri) => new Promise((resolve, reject) => {
     xhr({uri, responseType}, (error, response) => {
         if (error || response.statusCode !== 200) {
             return reject();
@@ -86,8 +86,8 @@ const fetchAs = responseType => uri => new Promise((resolve, reject) => {
 
 // These two helpers allow easy fetching of backpack code and sprite zips
 // Use the curried fetchAs here so the consumer does not worry about XHR responseTypes
-const fetchCode = fetchAs('json');
-const fetchSprite = fetchAs('arraybuffer');
+const fetchCode = fetchAs.bind(null, 'json');
+const fetchSprite = fetchAs.bind(null, 'arraybuffer');
 
 export {
     getBackpackContents,

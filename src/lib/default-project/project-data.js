d410ebeab2ba11db08c5df3204518b3602fca0d8
Katie Broida <kbroida@gmail.com>
2018-11-16 15:15:17 -0500

    Remove default project values for extensions that haven't been added to a project yet

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -62,10 +62,7 @@ const projectData = translateFunction => {
                         md5ext: '83a9787d4cb6f3b7632b4ddfebf74367.wav'
                     }
                 ],
-                volume: 100,
-                tempo: 60,
-                videoTransparency: 50,
-                videoState: 'off'
+                volume: 100
             },
             {
                 isStage: false,

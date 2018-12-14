97a3aab1a0537087fb507aafe12a8847a715cb54
Kouji Takao <kouji.takao@gmail.com>
2018-12-14 14:42:56 +0900

    add tests for event_whenflagclicked.

--- test/unit/lib/ruby-to-blocks-converter/event.test.js
+++ test/unit/lib/ruby-to-blocks-converter/event.test.js
@@ -8,49 +8,84 @@ import {
 describe('RubyToBlocksConverter/Event', () => {
     let converter;
     let target;
+    let code;
+    let expected;
 
     beforeEach(() => {
         converter = new RubyToBlocksConverter(null);
         target = null;
+        code = null;
+        expected = null;
     });
 
-    test('event_whenflagclicked', () => {
-        let code;
-        let expected;
-
-        code = 'self.when(:flag_clicked) { bounce_if_on_edge }';
-        expected = [
-            {
-                opcode: 'event_whenflagclicked',
-                next: {
-                    opcode: 'motion_ifonedgebounce'
+    describe('event_whenflagclicked', () => {
+        test('normal', () => {
+            code = 'self.when(:flag_clicked) { bounce_if_on_edge }';
+            expected = [
+                {
+                    opcode: 'event_whenflagclicked',
+                    next: {
+                        opcode: 'motion_ifonedgebounce'
+                    }
                 }
-            }
-        ];
-        convertAndExpectToEqualBlocks(converter, target, code, expected);
+            ];
+            convertAndExpectToEqualBlocks(converter, target, code, expected);
 
-        code = 'self.when(:flag_clicked) { bounce_if_on_edge; move(10) }';
-        expected = [
-            {
-                opcode: 'event_whenflagclicked',
-                next: rubyToExpected(converter, target, 'bounce_if_on_edge; move(10)')[0]
-            }
-        ];
-        convertAndExpectToEqualBlocks(converter, target, code, expected);
-
-        [
-            'self.when(:flag_clicked)'
-        ].forEach(s => {
-            convertAndExpectToEqualRubyStatement(converter, target, s, s);
+            code = 'self.when(:flag_clicked) { bounce_if_on_edge; move(10) }';
+            expected = [
+                {
+                    opcode: 'event_whenflagclicked',
+                    next: rubyToExpected(converter, target, 'bounce_if_on_edge; move(10)')[0]
+                }
+            ];
+            convertAndExpectToEqualBlocks(converter, target, code, expected);
         });
 
-        [
-            'self.when(:flag_clicked, 1) { bounce_if_on_edge }',
-            'self.when(:flag_click) { bounce_if_on_edge }'
-        ].forEach(s => {
-            expect(converter.targetCodeToBlocks(target, s)).toBeTruthy();
-            const blockId = Object.keys(converter.blocks).filter(id => converter.blocks[id].topLevel)[0];
-            expect(converter.blocks[blockId].opcode).toEqual('ruby_statement_with_block');
+        test('hat', () => {
+            code = `
+                bounce_if_on_edge
+                self.when(:flag_clicked) do
+                end
+                bounce_if_on_edge
+            `;
+            expected = [
+                rubyToExpected(converter, target, 'bounce_if_on_edge')[0],
+                {
+                    opcode: 'event_whenflagclicked'
+                },
+                rubyToExpected(converter, target, 'bounce_if_on_edge')[0]
+            ];
+            convertAndExpectToEqualBlocks(converter, target, code, expected);
+        });
+
+        test('invalid', () => {
+            [
+                'self.when(:flag_clicked)'
+            ].forEach(s => {
+                convertAndExpectToEqualRubyStatement(converter, target, s, s);
+            });
+
+            [
+                'self.when(:flag_clicked, 1) { bounce_if_on_edge }',
+                'self.when(:flag_click) { bounce_if_on_edge }'
+            ].forEach(s => {
+                expect(converter.targetCodeToBlocks(target, s)).toBeTruthy();
+                const blockId = Object.keys(converter.blocks).filter(id => converter.blocks[id].topLevel)[0];
+                expect(converter.blocks[blockId].opcode).toEqual('ruby_statement_with_block');
+            });
+        });
+
+        test('error', () => {
+            code = `
+                forever do
+                  self.when(:flag_clicked) do
+                  end
+                end
+            `;
+            const res = converter.targetCodeToBlocks(target, code);
+            expect(converter.errors).toHaveLength(1);
+            expect(converter.errors[0].row).toEqual(2);
+            expect(res).toBeFalsy();
         });
     });
 });

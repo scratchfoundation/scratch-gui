8a9996b24777d8b76375d5f34dfa837d2d7e5d8f
Ben Wheeler <wheeler.benjamin@gmail.com>
2018-12-19 19:45:38 -0500

    refactored font loading state to be in redux

--- src/lib/vm-manager-hoc.jsx
+++ src/lib/vm-manager-hoc.jsx
@@ -117,6 +117,7 @@ const vmManagerHOC = function (WrappedComponent) {
     const mapStateToProps = state => {
         const loadingState = state.scratchGui.projectState.loadingState;
         return {
+            fontsLoaded: state.scratchGui.fontsLoaded,
             isLoadingWithId: getIsLoadingWithId(loadingState),
             projectData: state.scratchGui.projectState.projectData,
             projectId: state.scratchGui.projectState.projectId,

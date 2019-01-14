2c62404bc803deeaf1613efd7f633406f0a95cc7
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2019-01-14 11:13:55 -0500

    Add Vernier extension tile for testing

--- src/lib/libraries/extensions/index.jsx
+++ src/lib/libraries/extensions/index.jsx
@@ -233,5 +233,34 @@ export default [
         ),
         helpLink: 'https://scratch.mit.edu/wedo'
 
+    },
+    {
+        name: 'GDX-FOR',
+        extensionId: 'gdxfor',
+        collaborator: 'Vernier',
+        iconURL: microbitImage,
+        insetIconURL: microbitMenuImage,
+        description: (
+            <FormattedMessage
+                defaultMessage="Connect your projects with the world."
+                description="Description for the 'GDX-FOR' extension"
+                id="gui.extension.gdxfor.description"
+            />
+        ),
+        featured: true,
+        disabled: false,
+        bluetoothRequired: true,
+        launchPeripheralConnectionFlow: true,
+        useAutoScan: false,
+        peripheralImage: microbitPeripheralImage,
+        smallPeripheralImage: microbitMenuImage,
+        connectingMessage: (
+            <FormattedMessage
+                defaultMessage="Connecting"
+                description="Message to help people connect to their micro:bit."
+                id="gui.extension.gdxfor.connectingMessage"
+            />
+        ),
+        helpLink: 'https://scratch.mit.edu/microbit'
     }
 ];

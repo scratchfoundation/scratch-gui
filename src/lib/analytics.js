33876158ec588673d00b64d3e52cd6e2689fb3b8
Sandy Enoch <alexander@robotical.io>
2019-01-21 18:35:20 +0000

    stop analytics initialising, required for running offline

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,10 +1,12 @@
 import GoogleAnalytics from 'react-ga';
 
+/*
 GoogleAnalytics.initialize(process.env.GA_ID, {
     debug: (process.env.NODE_ENV !== 'production'),
     titleCase: true,
     sampleRate: (process.env.NODE_ENV === 'production') ? 100 : 0,
     forceSSL: true
 });
+*/
 
 export default GoogleAnalytics;

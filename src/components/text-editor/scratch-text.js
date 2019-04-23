fd677d53062dce21dbbdb12b82b1851fb217b789
AlbertSnows <ajsnow@ksu.edu>
2019-04-23 16:49:11 -0500

    tokenize

---
+++ src/components/text-editor/scratch-text.js
@@ -0,0 +1,109 @@
+/**
+ * This is the language definition file for our Monaco text editor for scratch.
+ * We need this file so Monaco can identify the keywords for scratch blocks that are text.
+ * Examples for each language can be found here:
+ * https://microsoft.github.io/monaco-editor/playground.html#extending-language-services-custom-languages
+ */
+import ScratchBlocks from 'scratch-blocks';
+// console.log(ScratchBlocks.msg.HELP);
+let defs = {
+    // Set defaultToken to invalid to see what you do not tokenize yet
+    // defaultToken: 'invalid',
+
+    // /when key (\w+) is pressed/
+    keywords: [
+
+    ],
+
+    // we include these common regular expressions
+    symbols: /[=><!~?:&|+\-*/^%]+/,
+
+    tokenizer: {
+        root: [
+            {include: '@whitespace'},
+            {include: '@numbers'},
+            {include: '@strings'},
+
+            [/[,:;]/, 'delimiter'],
+            [/[{}[\]()]/, '@brackets'],
+
+            [/@[a-zA-Z]\w*/, 'tag'],
+            [/[a-zA-Z]\w*/, {
+                cases: {
+                    '@keywords': 'keyword',
+                    '@default': 'identifier'
+                }
+            }]
+        ],
+
+        // Deal with white space, including single and multi-line comments
+        whitespace: [
+            [/\s+/, 'white'],
+            [/(^#.*$)/, 'comment'],
+            [/('''.*''')|(""".*""")/, 'string'],
+            [/'''.*$/, 'string', '@endDocString'],
+            [/""".*$/, 'string', '@endDblDocString']
+        ],
+        endDocString: [
+            [/\\'/, 'string'],
+            [/.*'''/, 'string', '@popall'],
+            [/.*$/, 'string']
+        ],
+        endDblDocString: [
+            [/\\"/, 'string'],
+            [/.*"""/, 'string', '@popall'],
+            [/.*$/, 'string']
+        ],
+
+        // Recognize hex, negatives, decimals, imaginaries, longs, and scientific notation
+        numbers: [
+            [/-?0x([abcdef]|[ABCDEF]|\d)+[lL]?/, 'number.hex'],
+            [/-?(\d*\.)?\d+([eE][+-]?\d+)?[jJ]?[lL]?/, 'number']
+        ],
+
+        // Recognize strings, including those broken across lines with \ (but not without)
+        strings: [
+            [/'$/, 'string.escape', '@popall'],
+            [/'/, 'string.escape', '@stringBody'],
+            [/"$/, 'string.escape', '@popall'],
+            [/"/, 'string.escape', '@dblStringBody']
+        ],
+        stringBody: [
+            [/[^\\']+$/, 'string', '@popall'],
+            [/[^\\']+/, 'string'],
+            [/\\./, 'string'],
+            [/'/, 'string.escape', '@popall'],
+            [/\\$/, 'string']
+        ],
+        dblStringBody: [
+            [/[^\\"]+$/, 'string', '@popall'],
+            [/[^\\"]+/, 'string'],
+            [/\\./, 'string'],
+            [/"/, 'string.escape', '@popall'],
+            [/\\$/, 'string']
+        ]
+    }
+};
+
+/**
+ * Populates defs with all the block type definitons for monaco to work with. Defs is referenced from:
+ * https://microsoft.github.io/monaco-editor/monarch.html
+ * @returns {array} should hold a list of all blocks as defined in this file:
+ * https://github.com/KSU-CS-Software-Engineering/scratch-blocks/blob/develop/msg/messages.js
+ */
+export default function LangDef (){
+    Object.keys(ScratchBlocks.Msg).forEach(msg => {
+        // For some example, say msg: CONTROL_FOREACH
+        // Get the class
+        const className = msg.split('_')[0].toLowerCase(); // => event
+        // Get Msg at [msg]
+        const textRepresentation = ScratchBlocks.Msg[msg]; // => Blockly.Msg.CONTROL_FOREACH = 'for each %1 in %2';
+        // Convert value to RegExp when %1 clicked => ^when (it?)clicked$
+        // Regulat expresson '.+' = One or more of any character except line break
+        const regExpStr = textRepresentation.replace(/%\d+/g, '.+'); // => 'for each '.+' in '.+'
+        if (!defs.tokenizer[className]) defs.tokenizer[className] = [];
+        // DO NEXT - save as a def file
+        defs.tokenizer[className].push([new RegExp(regExpStr), className]); // => ['for each '.+' in '.+', control]
+    });
+    return defs;
+}
--- src/components/text-editor/text-editor.jsx
+++ src/components/text-editor/text-editor.jsx
@@ -1,6 +1,12 @@
+/**
+ * Text editor file with monaco information.
+ * For communication with scratch-text.js, look here:
+ * https://microsoft.github.io/monaco-editor/playground.html#extending-language-services-custom-languages
+ */
 import React from 'react';
 import {injectIntl} from 'react-intl';
 import MonacoEditor from 'react-monaco-editor';
+import LangDef from './scratch-text';
 import styles from './text-editor.css';
 
 const options = {
@@ -13,12 +19,65 @@ class TextEditor extends React.Component {
         this.state = {
             blocks: {}
         };
+
     }
+
 
     componentDidUpdate (prevProps) {
         if (this.props.blocks !== prevProps.blocks) {
             this.setState({blocks: this.props.blocks});
         }
+    }
+
+    editorWillMount (monaco) {
+        monaco.languages.register({id: 'scratch-text'});
+        monaco.languages.setMonarchTokensProvider('scratch-text', LangDef());
+        monaco.languages.setMonarchTokensProvider('scratch-text', {
+            tokenizer: {
+                root: [
+                    LangDef.defs
+                ]
+            }
+        });
+        monaco.editor.defineTheme('scratch-text-theme', {
+            base: 'scratch-text',
+            inherit: false,
+            rules: [
+                {token: 'custom-info', foreground: '808080'},
+                {token: 'custom-error', foreground: 'ff0000', fontStyle: 'bold'},
+                {token: 'custom-notice', foreground: 'FFA500'},
+                {token: 'custom-date', foreground: '008800'}]
+        });
+        monaco.languages.registerCompletionItemProvider('scratch-text', {provideCompletionItems: () => {
+            const suggestions = [{
+                label: 'simpleText',
+                kind: monaco.languages.CompletionItemKind.Text,
+                insertText: 'simpleText'
+            }, {
+                label: 'testing',
+                kind: monaco.languages.CompletionItemKind.Keyword,
+                insertText: 'testing(${1:condition})',
+                insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet
+            }, {
+                label: 'ifelse',
+                kind: monaco.languages.CompletionItemKind.Snippet,
+                insertText: [
+                    'if (${1:condition}) {',
+                    '\t$0',
+                    '} else {',
+                    '\t',
+                    '}'
+                ].join('\n'),
+                insertTextRules: monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+                documentation: 'If-Else Statement'
+            }];
+            return {suggestions: suggestions};
+        }});
+        monaco.editor.create(document.getElementById('container'), {
+            theme: 'scratch-text-theme',
+            value: 'getCode()',
+            language: 'scratch-text'
+        });
     }
 
     displayBlocks (blocks) {
@@ -46,8 +105,9 @@ class TextEditor extends React.Component {
         return (
             <div className={styles.editorContainer}>
                 <MonacoEditor
+                    editorWillMount={this.editorWillMount}
                     height="100%"
-                    language="javascript"
+                    language="scratch-text"
                     options={options}
                     theme="vs-dark"
                     value={blocksText}

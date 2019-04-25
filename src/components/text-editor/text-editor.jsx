8f5ee54502de82267da93aeef1bc179653a55328
Thomas Rolston <trolston@ksu.edu>
2019-04-25 16:44:56 -0500

    Added comments to everything relating to the text tab

--- src/components/text-editor/text-editor.jsx
+++ src/components/text-editor/text-editor.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import {injectIntl} from 'react-intl';
 import MonacoEditor from 'react-monaco-editor';
 import styles from './text-editor.css';
+import PropTypes from 'prop-types';
 
-const options = {
-    automaticLayout: true
-};
-
+/**
+ * This is the component for displaying the text tab.  The monaco editor is displayed
+ * and the blocks are converted to a text format and put into the monaco editor.
+ */
 class TextEditor extends React.Component {
+
+    /**
+     * Intializes the TextEditor class
+     *
+     * @param {PropTypes} props The props passed in to the TextEditor
+     */
     constructor (props) {
         super(props);
         this.state = {
@@ -15,40 +22,77 @@ class TextEditor extends React.Component {
         };
     }
 
+    /**
+     * Updates the current set of blocks.  This is called after the VM is rendered and
+     * there are changes to the blocks.
+     *
+     * @param {React.Props} prevProps The old props
+     */
     componentDidUpdate (prevProps) {
         if (this.props.blocks !== prevProps.blocks) {
-            this.setState({blocks: this.props.blocks});
+            this.updateBlocks(this.props.blocks);
         }
     }
 
+    /**
+     * Sets the current state of the blocks to the passed in blocks object.
+     *
+     * @param {object} blocks The object of blocks to update the current state to
+     */
+    updateBlocks (blocks) {
+        this.setState({blocks});
+    }
+
+    /**
+     * Converts the current list of blocks into a text string
+     *
+     * @param {object} blocks The current list of blocks
+     *
+     * @returns {string} A string of the object of blocks converted to text.
+     */
     displayBlocks (blocks) {
+        // Defaults the text string ofblocks
         let blocksText = '';
+        // Gets the number of blocks in the blocks object
         const blocksLength = Object.keys(blocks).length;
+        // If there are blocks
         if (blocksLength > 0) {
+            // For each block
             for (const blockKey in blocks) {
+                // Get the current block
                 const block = blocks[blockKey];
+                // Check if the current block is a movesteps block
                 if (block.opcode === 'motion_movesteps') {
+                    // Get the input value of the steps to move
                     const inputValue = block.inputs.STEPS[1][1];
+                    // Add the proper outputted string to the blocks text with the input value
                     blocksText += `Move (${inputValue}) steps\n`;
+                // If the block is a turnright block
                 } else if (block.opcode === 'motion_turnright') {
+                    // Get the input value
                     const inputValue = block.inputs.DEGREES[1][1];
+                    // Add the properly formatted string to the string of converted blocks
                     blocksText += `Turn (${inputValue}) degrees right\n`;
                 }
             }
         }
+        // Return the formatted string of blocks
         return blocksText;
     }
 
     render () {
+        // Gets the current list of blocks in the state
         const {blocks} = this.state;
+        // Converts the current list of blocks into a formatted string
         const blocksText = this.displayBlocks(blocks);
 
+        // Create the monaco editor passing in the converted string of blocks to be defaulted
         return (
             <div className={styles.editorContainer}>
                 <MonacoEditor
                     height="100%"
                     language="javascript"
-                    options={options}
+                    options={{automaticLayout: true}}
                     theme="vs-dark"
                     value={blocksText}
                     width="100%"
@@ -57,5 +101,9 @@ class TextEditor extends React.Component {
         );
     }
 }
+
+TextEditor.propTypes = {
+    blocks: PropTypes.object
+};
 
 export default injectIntl(TextEditor);
--- src/containers/text-editor.jsx
+++ src/containers/text-editor.jsx
@@ -5,7 +5,15 @@ import PropTypes from 'prop-types';
 
 import {getBlocksFromVm} from '../lib/text';
 
+
+/**
+ * The wrapper class for the text editor.
+ */
 class TextEditor extends React.Component {
+    /**
+     * Constructs the TextEditor component
+     * @param {React.Props} props The properties passed in to this component
+     */
     constructor (props) {
         super(props);
         this.state = {
@@ -13,17 +21,27 @@ class TextEditor extends React.Component {
         };
     }
 
+    /**
+     * When the component is mounted this function is called to bind
+     * to the @function workspaceUpdate function in the VM
+     */
     componentDidMount () {
+        // Bind ot the 'workspaceUpdate' function from the VM in the props
         this.props.vm.on('workspaceUpdate', () => {
+            // Convert the VM from string to JSON
             const vmJson = JSON.parse(this.props.vm.toJSON());
 
+            // Get the current list of blocks from the VM JSON object
             getBlocksFromVm(vmJson, (err, blocks) => {
+                // Handle if there are any errors
                 if (err) throw err;
+                // Set the current state of blocks to the retrieved value
                 this.setState({blocks});
             });
         });
     }
 
+    
     render () {
         const {blocks} = this.state;
 

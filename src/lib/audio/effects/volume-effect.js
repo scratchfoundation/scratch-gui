1906208a78ba0f0177b40f9384f1fe7108e3f1e3
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2019-05-08 17:42:36 -0400

    Ramp the volume effect outside the selection

--- src/lib/audio/effects/volume-effect.js
+++ src/lib/audio/effects/volume-effect.js
@@ -7,20 +7,12 @@ class VolumeEffect {
 
         this.gain = this.audioContext.createGain();
 
+        // Smoothly ramp the gain up before the start time, and down after the end time.
         this.rampLength = 0.01;
-        if ((endSeconds - startSeconds) > this.rampLength * 2) {
-            // Smoothly ramp the gain up after the start time, and down before the end time.
-            this.gain.gain.setValueAtTime(1.0, startSeconds);
-            this.gain.gain.exponentialRampToValueAtTime(volume, startSeconds + this.rampLength);
-            this.gain.gain.setValueAtTime(volume, endSeconds - this.rampLength);
-            this.gain.gain.exponentialRampToValueAtTime(1.0, endSeconds);
-        } else {
-            // If the selection is shorter than twice the ramp length, set the gain at the
-            // start and end times without a smooth ramp.
-            this.gain.gain.setValueAtTime(volume, startSeconds);
-            this.gain.gain.setValueAtTime(1.0, endSeconds);
-        }
-
+        this.gain.gain.setValueAtTime(1.0, Math.max(0, startSeconds - this.rampLength));
+        this.gain.gain.exponentialRampToValueAtTime(volume, startSeconds);
+        this.gain.gain.setValueAtTime(volume, endSeconds);
+        this.gain.gain.exponentialRampToValueAtTime(1.0, endSeconds + this.rampLength);
 
         this.input.connect(this.gain);
         this.gain.connect(this.output);

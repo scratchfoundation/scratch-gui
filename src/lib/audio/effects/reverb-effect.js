8e07f2e5dd5288e839c78a5374f45ee3b58ae413
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2019-05-11 22:13:15 -0400

    use start and end times in reverb effect

--- src/lib/audio/effects/reverb-effect.js
+++ src/lib/audio/effects/reverb-effect.js
@@ -3,12 +3,17 @@ class ReverbEffect {
         this.audioContext = audioContext;
 
         this.input = this.audioContext.createGain();
-        this.output = this.audioContext.createGain();
-
+        this.effectInput = this.audioContext.createGain();
         this.effect = this.audioContext.createConvolver();
         this.effect.buffer = impulseResponseBuffer;
+        this.output = this.audioContext.createGain();
 
-        this.input.connect(this.effect);
+        this.effectInput.gain.value = 0;
+        this.effectInput.gain.setValueAtTime(1, startSeconds);
+        this.effectInput.gain.setValueAtTime(0, endSeconds);
+
+        this.input.connect(this.effectInput);
+        this.effectInput.connect(this.effect);
         this.effect.connect(this.output);
 
         this.input.connect(this.output);

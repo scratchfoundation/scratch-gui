a42dfab53e82cff79767e2f7d8e6ec3e4f9b94a3
Chris Garrity <chrisg@media.mit.edu>
2019-06-10 10:53:04 -0400

    Modify tile delete button for touch
    Prototype of larger delete button for sprites, costumes and sounds to better support touch devices.
    
    I split it out from `CloseButton` as it made the changes simpler, and we may end up going in a different direction (open context menu).

---
+++ src/components/delete-button/delete-button.jsx
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import classNames from 'classnames';
+
+import styles from './delete-button.css';
+import deleteIcon from './icon--delete.svg';
+
+const DeleteButton = props => (
+    <div
+        aria-label="Delete"
+        className={classNames(
+            styles.deleteButton,
+            props.className
+        )}
+        role="button"
+        tabIndex="0"
+        onClick={props.onClick}
+    >
+        <div className={styles.deleteButtonVisible}>
+            <img
+                className={styles.deleteIcon}
+                src={deleteIcon}
+            />
+        </div>
+    </div>
+
+);
+
+DeleteButton.propTypes = {
+    className: PropTypes.string,
+    onClick: PropTypes.func.isRequired
+};
+
+export default DeleteButton;

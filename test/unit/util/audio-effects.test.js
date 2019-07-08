846e4e43c28acb0006b702860be541bbf71cc196
Eric Rosenbaum <eric.rosenbaum@gmail.com>
2019-07-08 13:57:36 -0400

    Revert and fix tests
    This reverts commit 42ff50300cff7558fd03cdebb757eca8caf85199.

--- test/unit/util/audio-effects.test.js
+++ test/unit/util/audio-effects.test.js
@@ -14,17 +14,19 @@ describe('Audio Effects manager', () => {
     const audioBuffer = audioContext.createBuffer(1, 400, 44100);
 
     test('changes buffer length and playback rate for faster effect', () => {
-        const audioEffects = new AudioEffects(audioBuffer, 'faster', 0, 1);
+        const audioEffects = new AudioEffects(audioBuffer, 'faster');
         expect(audioEffects.audioContext._.length).toBeLessThan(400);
+        expect(audioEffects.source.playbackRate.value).toBeGreaterThan(1);
     });
 
     test('changes buffer length  and playback rate for slower effect', () => {
-        const audioEffects = new AudioEffects(audioBuffer, 'slower', 0, 1);
+        const audioEffects = new AudioEffects(audioBuffer, 'slower');
         expect(audioEffects.audioContext._.length).toBeGreaterThan(400);
+        expect(audioEffects.source.playbackRate.value).toBeLessThan(1);
     });
 
     test('changes buffer length for echo effect', () => {
-        const audioEffects = new AudioEffects(audioBuffer, 'echo', 0, 1);
+        const audioEffects = new AudioEffects(audioBuffer, 'echo');
         expect(audioEffects.audioContext._.length).toBeGreaterThan(400);
     });
 
@@ -41,15 +43,15 @@ describe('Effects', () => {
     });
 
     test('all effects provide an input and output that are connected', () => {
-        const robotEffect = new RobotEffect(audioContext, 0.5, 0, 0.01);
+        const robotEffect = new RobotEffect(audioContext, 0.5);
         expect(robotEffect.input).toBeInstanceOf(AudioNode);
         expect(robotEffect.output).toBeInstanceOf(AudioNode);
 
-        const echoEffect = new EchoEffect(audioContext, 0.5, 0, 0.01);
+        const echoEffect = new EchoEffect(audioContext, 0.5);
         expect(echoEffect.input).toBeInstanceOf(AudioNode);
         expect(echoEffect.output).toBeInstanceOf(AudioNode);
 
-        const volumeEffect = new VolumeEffect(audioContext, 0.5, 0, 0.01);
+        const volumeEffect = new VolumeEffect(audioContext, 0.5);
         expect(volumeEffect.input).toBeInstanceOf(AudioNode);
         expect(volumeEffect.output).toBeInstanceOf(AudioNode);
     });

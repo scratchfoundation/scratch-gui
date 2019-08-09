872b198f35c42955ff23486abf682fcfaffedd9e
Paul Kaplan <pkaplan@media.mit.edu>
2019-08-09 11:45:26 -0400

    WIP canvas super waveform

--- src/components/record-modal/recording-step.jsx
+++ src/components/record-modal/recording-step.jsx
@@ -1,9 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import {defineMessages, injectIntl, intlShape} from 'react-intl';
-import Box from '../box/box.jsx';
 import Meter from '../meter/meter.jsx';
-import Waveform from '../waveform/waveform.jsx';
 
 import styles from './record-modal.css';
 import stopIcon from './icon--stop-recording.svg';
@@ -32,24 +30,20 @@ const messages = defineMessages({
 });
 
 const RecordingStep = props => (
-    <Box>
-        <Box className={styles.visualizationContainer}>
-            <Box className={styles.meterContainer}>
+    <div>
+        <div className={styles.visualizationContainer}>
+            <div className={styles.meterContainer}>
                 <Meter
                     height={172}
                     level={props.level}
                     width={20}
                 />
-            </Box>
-            <Box className={styles.waveformContainer}>
-                {props.levels ? (
-                    <Waveform
-                        data={props.levels}
-                        height={150}
-                        level={0}
-                        width={440}
-                    />
-                ) : (
+            </div>
+            <div
+                className={styles.waveformContainer}
+                ref={props.visRef}
+            >
+                {!props.recording && (
                     <span className={styles.helpText}>
                         {props.listening ? props.intl.formatMessage(messages.beginRecord) :
                             props.intl.formatMessage(messages.permission,
@@ -58,9 +52,9 @@ const RecordingStep = props => (
                         }
                     </span>
                 )}
-            </Box>
-        </Box>
-        <Box className={styles.mainButtonRow}>
+            </div>
+        </div>
+        <div className={styles.mainButtonRow}>
             <button
                 className={styles.mainButton}
                 disabled={!props.listening}
@@ -101,19 +95,19 @@ const RecordingStep = props => (
                     </span>
                 </div>
             </button>
-        </Box>
-    </Box>
+        </div>
+    </div>
 );
 
 RecordingStep.propTypes = {
     intl: intlShape.isRequired,
     isRtl: PropTypes.bool,
     level: PropTypes.number,
-    levels: PropTypes.arrayOf(PropTypes.number),
     listening: PropTypes.bool,
     onRecord: PropTypes.func.isRequired,
     onStopRecording: PropTypes.func.isRequired,
-    recording: PropTypes.bool
+    recording: PropTypes.bool,
+    visRef: PropTypes.func.isRequired
 };
 
 export default injectIntl(RecordingStep);
--- src/containers/recording-step.jsx
+++ src/containers/recording-step.jsx
@@ -4,6 +4,7 @@ import bindAll from 'lodash.bindall';
 import RecordingStepComponent from '../components/record-modal/recording-step.jsx';
 import AudioRecorder from '../lib/audio/audio-recorder.js';
 import {defineMessages, injectIntl, intlShape} from 'react-intl';
+import Waveform from '../components/waveform/waveform.jsx';
 
 const messages = defineMessages({
     alertMsg: {
@@ -17,6 +18,7 @@ class RecordingStep extends React.Component {
     constructor (props) {
         super(props);
         bindAll(this, [
+            'setVisRef',
             'handleRecord',
             'handleStopRecording',
             'handleStarted',
@@ -44,18 +46,67 @@ class RecordingStep extends React.Component {
         alert(this.props.intl.formatMessage(messages.alertMsg)); // eslint-disable-line no-alert
     }
     handleLevelUpdate (level) {
-        this.setState({level});
         if (this.props.recording) {
-            this.setState({levels: (this.state.levels || []).concat([level])});
+            const levels = this._levels || [];
+            levels.push(level);
+            // if (levels.length > WAVEFORM_WINDOW) levels.shift();
+            this._levels = levels;
+            this.drawRecording();
+        } else {
+            this.setState({level});
         }
+        // this.setState({
+        //     level: level,
+        //     levels: this.props.recording ? (this.state.levels || []).concat([level]) : this.state.levels
+        // });
     }
     handleRecord () {
         this.audioRecorder.startRecording();
         this.props.onRecord();
+        this._levels = [];
     }
     handleStopRecording () {
+        this.visRef.remove(this._canvas);
         const {samples, sampleRate, levels, trimStart, trimEnd} = this.audioRecorder.stop();
         this.props.onStopRecording(samples, sampleRate, levels, trimStart, trimEnd);
+    }
+    drawRecording () {
+        if (!this._canvas) {
+            const bbox = this.visRef.getBoundingClientRect();
+            this._canvas = document.createElement('canvas');
+            this._canvas.width = bbox.width;
+            this._canvas.height = bbox.height;
+            this.visRef.appendChild(this._canvas);
+        }
+        const ctx = this._canvas.getContext('2d');
+        const {width, height} = this._canvas;
+
+        const pathComponents = Waveform.pathComponents(width, height, this._levels);
+        
+        ctx.fillStyle = 'hsl(300, 54%, 72%)';
+        ctx.strokeStyle = '#BD42BD';
+        ctx.clearRect(0, 0, width, height);
+        ctx.beginPath();
+        ctx.moveTo(0, height / 2);
+        for (let i = 0; i < pathComponents.length; i++) {
+            const cmd = pathComponents[i];
+            switch (cmd[0]) {
+            case 'M':
+                ctx.moveTo(cmd[1], cmd[2]);
+                break;
+            case 'Q':
+                ctx.quadraticCurveTo(cmd[1], cmd[2], cmd[3], cmd[4]);
+                break;
+            case 'Z':
+                ctx.closePath();
+                break;
+            }
+        }
+        ctx.fill();
+        ctx.stroke();
+    }
+    setVisRef (ref) {
+        this.visRef = ref;
     }
     render () {
         const {
@@ -65,8 +116,8 @@ class RecordingStep extends React.Component {
         } = this.props;
         return (
             <RecordingStepComponent
+                visRef={this.setVisRef}
                 level={this.state.level}
-                levels={this.state.levels}
                 listening={this.state.listening}
                 onRecord={this.handleRecord}
                 onStopRecording={this.handleStopRecording}

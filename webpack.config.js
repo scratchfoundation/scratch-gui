574b7d50cc6bb961b51f6e8f2748d704b363075c
SheepTester <ussa.say@gmail.com>
2019-12-23 17:53:34 -0800

    Change name of mod to E羊iques

--- webpack.config.js
+++ webpack.config.js
@@ -142,7 +142,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch 3.0 GUI',
+                title: 'E羊icques: A Scratch mod with an alternative aspect ratio',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({

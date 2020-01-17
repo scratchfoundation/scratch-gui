00b0bf4249c79f43cc6f12539003c098e97f07e2
Sakamoto, Kazunori <exkazuu@gmail.com>
2020-01-17 14:55:03 +0900

    Upgrade loader packages fixing settings

--- webpack.config.js
+++ webpack.config.js
@@ -65,10 +65,11 @@ const base = {
             }, {
                 loader: 'css-loader',
                 options: {
-                    modules: true,
                     importLoaders: 1,
-                    localIdentName: '[name]_[local]_[hash:base64:5]',
-                    camelCase: true
+                    localsConvention: 'camelCase',
+                    modules: {
+                        localIdentName: '[name]_[local]_[hash:base64:5]'
+                    }
                 }
             }, {
                 loader: 'postcss-loader',

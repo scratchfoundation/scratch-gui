b6a4ac97d0958a024698559e3c553067e025b651
rikutons <rikutonsicloud@gmail.com>
2020-01-24 21:42:37 +0900

    [CHANGE] index.js's unnatural tab to space

--- src/lib/ruby-to-blocks-converter/index.js
+++ src/lib/ruby-to-blocks-converter/index.js
@@ -37,7 +37,7 @@ class RubyToBlocksConverter {
             VariablesConverter,
             MyBlocksConverter,
             MicroBitConverter,
-			MusicConverter,
+            MusicConverter,
             EV3Converter
         ];
         this.reset();

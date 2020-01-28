e91713c2621559ad04b38a6c0d3af89fed3c8e6b
Christopher Willis-Ford <cwillisf@media.mit.edu>
2020-01-27 17:28:15 -0800

    initialize Google Analytics only if GA_ID is set

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,10 +1,13 @@
 import GoogleAnalytics from 'react-ga';
 
-GoogleAnalytics.initialize(process.env.GA_ID || window.GA_ID, {
-    debug: (process.env.NODE_ENV !== 'production'),
-    titleCase: true,
-    sampleRate: (process.env.NODE_ENV === 'production') ? 100 : 0,
-    forceSSL: true
-});
+const GA_ID = (process.env.GA_ID || window.GA_ID);
+if (GA_ID) {
+    GoogleAnalytics.initialize(GA_ID, {
+        debug: (process.env.NODE_ENV !== 'production'),
+        titleCase: true,
+        sampleRate: (process.env.NODE_ENV === 'production') ? 100 : 0,
+        forceSSL: true
+    });
+}
 
 export default GoogleAnalytics;

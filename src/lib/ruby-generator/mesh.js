0f43d6afbfa5d16fdf5164e8b9637f877e7b7733
Kouji Takao <kouji.takao@gmail.com>
2020-04-14 20:25:10 +0900

    mesh block -> ruby.

---
+++ src/lib/ruby-generator/mesh.js
@@ -0,0 +1,18 @@
+/**
+ * Define Ruby code generator for Mesh Blocks
+ * @param {RubyGenerator} Generator The RubyGenerator
+ * @return {RubyGenerator} same as param.
+ */
+export default function (Generator) {
+    Generator.mesh_menu_variableNames = function (block) {
+        const name = Generator.quote_(Generator.getFieldValue(block, 'variableNames') || ' ');
+        return [name, Generator.ORDER_ATOMIC];
+    };
+
+    Generator.mesh_getSensorValue = function (block) {
+        const name = Generator.valueToCode(block, 'NAME', Generator.ORDER_NONE) || '" "';
+        return `mesh_sensor_value(${name})\n`;
+    };
+
+    return Generator;
+}

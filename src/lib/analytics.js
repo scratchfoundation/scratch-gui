0e052e20ce383afbb8360078ca85fcd19cfcb1d0
jz1code <juzi@LAPTOP-LK684E16.localdomain>
2020-05-14 09:15:10 +0800

    注释掉ga

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,10 +1,10 @@
 import GoogleAnalytics from 'react-ga';
 
-GoogleAnalytics.initialize(process.env.GA_ID || window.GA_ID, {
-    debug: (process.env.NODE_ENV !== 'production'),
-    titleCase: true,
-    sampleRate: (process.env.NODE_ENV === 'production') ? 100 : 0,
-    forceSSL: true
-});
+// GoogleAnalytics.initialize(process.env.GA_ID || window.GA_ID, {
+//     debug: (process.env.NODE_ENV !== 'production'),
+//     titleCase: true,
+//     sampleRate: (process.env.NODE_ENV === 'production') ? 100 : 0,
+//     forceSSL: true
+// });
 
 export default GoogleAnalytics;

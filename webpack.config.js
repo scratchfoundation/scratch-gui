48e8f90c61e2ad48e793eccb6f9f703f422d9190
Thomas Weber <tommyweber33@gmail.com>
2020-07-26 18:03:45 -0500

    Update title of the index

--- webpack.config.js
+++ webpack.config.js
@@ -137,7 +137,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'TurboWarp',
+                title: 'TurboWarp - Run Scratch projects faster',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({

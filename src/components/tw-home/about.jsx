d39b958c598c9912cc2dcb01bd07bcdc6f6800fd
Thomas Weber <tommyweber33@gmail.com>
2020-08-11 23:13:50 -0500

    Show a warning to firefox users regarding performance
    Chrome quicksort on 200,000: 0.1804
    Firefox quicksort on 200,000: 0.8003
    
    So actually more than 4x faster...
    https://bugzilla.mozilla.org/show_bug.cgi?id=1317690

--- src/components/tw-home/about.jsx
+++ src/components/tw-home/about.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-const About = () => {
-    return (
+const isFirefox = /Firefox/.test(navigator.userAgent);
+
+const About = () => (
+    <React.Fragment>
         <p>
             <FormattedMessage
                 defaultMessage="TurboWarp compiles Scratch projects to JavaScript to make them run faster. Try it out by inputting a project ID or URL and pressing enter. Visit {studioLink} for some examples."
@@ -21,7 +23,16 @@ const About = () => {
                 }}
             />
         </p>
-    );
-};
+        <p>
+            {isFirefox ? (
+                <FormattedMessage
+                    defaultMessage="Firefox users: Use Google Chrome for up to 4x better performance (see Bugzilla ID 1317690)"
+                    description="Warning for firefox users about performance."
+                    id="tw.home.ffWarning"
+                />
+            ) : null}
+        </p>
+    </React.Fragment>
+);
 
 export default About;

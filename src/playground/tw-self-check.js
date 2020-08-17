2016e0c419e656b9726a16cc11095ae062bbeef7
Thomas Weber <tommyweber33@gmail.com>
2020-08-16 19:51:20 -0500

    Update self check

--- src/playground/tw-self-check.js
+++ src/playground/tw-self-check.js
@@ -7,11 +7,22 @@ import VirtualMachine from 'scratch-vm';
 import RenderWebGL from 'scratch-render';
 
 if (!VirtualMachine.isTurboWarp) {
-    alert('The installed scratch-vm is not the TurboWarp fork.');
     console.error('The installed scratch-vm is not the TurboWarp fork.');
+    alert('The installed scratch-vm is not the TurboWarp fork.');
 }
 
 if (!RenderWebGL.isTurboWarp) {
-    alert('The installed scratch-render is not the TurboWarp fork.');
     console.error('The installed scratch-render is not the TurboWarp fork.');
+    alert('The installed scratch-render is not the TurboWarp fork.');
+}
+
+var _twSelfCheck = 'bad';
+try {
+    eval('_twSelfCheck = "ok"');
+} catch (e) { /* ignore */ }
+if (_twSelfCheck !== 'ok') {
+    // An adblocker or something is breaking eval()
+    // Could also be Content-Security-Policy or something like that.
+    console.error('eval() is broken. Most likely caused by a browser extension or your CSP.');
+    alert('There is something wrong with how your browser is configured. Try turning off any adblockers and refreshing, otherwise TurboWarp will not work. (technical information: eval() is broken)');
 }

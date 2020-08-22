1fae40571279f5f9614dbccbc531b80995009bb2
Thomas Weber <tommyweber33@gmail.com>
2020-08-22 14:38:45 -0500

    Remove firefox performance warning
    To be clear, Chrome is still faster. 2x faster in fact.
    But the difference has been significantly mitigated
    through some VM optimizations, and that
    bug ID is less relavent now.

--- src/components/tw-home/about.jsx
+++ src/components/tw-home/about.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-const isFirefox = /Firefox/.test(navigator.userAgent);
-
 const About = () => (
     <React.Fragment>
         <p>
@@ -11,15 +9,6 @@ const About = () => (
                 description="Description of TurboWarp"
                 id="tw.home.description"
             />
-        </p>
-        <p>
-            {isFirefox ? (
-                <FormattedMessage
-                    defaultMessage="Firefox users: Use Google Chrome for up to 4x better performance (see Bugzilla ID 1317690)"
-                    description="Warning for firefox users about performance."
-                    id="tw.home.ffWarning"
-                />
-            ) : null}
         </p>
     </React.Fragment>
 );

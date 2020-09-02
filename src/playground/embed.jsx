486e302a9920b8e795bf3a3d2eda6397cc3623f9
Thomas Weber <weber_t@mailbox.org>
2020-09-01 19:45:04 -0500

    Embed should not use TWParserHOC

--- src/playground/embed.jsx
+++ src/playground/embed.jsx
@@ -3,7 +3,6 @@ import './tw-imports';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {compose} from 'redux';
-import TWParserHOC from '../lib/tw-parser-hoc.jsx';
 import AppStateHOC from '../lib/app-state-hoc.jsx';
 
 import GUI from './render-gui.jsx';
@@ -16,8 +15,7 @@ const searchParams = new URLSearchParams(location.search);
 const projectId = searchParams.get('id');
 
 const WrappedGUI = compose(
-    AppStateHOC,
-    TWParserHOC
+    AppStateHOC
 )(GUI);
 
 ReactDOM.render(<WrappedGUI

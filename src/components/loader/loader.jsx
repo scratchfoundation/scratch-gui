bdb808b98b4431b98cfc6f311cbf804b5e8acec7
Thomas Weber <weber_t@mailbox.org>
2020-09-10 19:49:31 -0500

    Update progress bar

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -7,98 +7,7 @@ import PropTypes from 'prop-types';
 import topBlock from './top-block.svg';
 import middleBlock from './middle-block.svg';
 import bottomBlock from './bottom-block.svg';
-const messages = [
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Creating blocks …"
-                description="One of the loading messages"
-                id="gui.loader.message1"
-            />
-        ),
-        weight: 50
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Loading sprites …"
-                description="One of the loading messages"
-                id="gui.loader.message2"
-            />
-        ),
-        weight: 50
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Loading sounds …"
-                description="One of the loading messages"
-                id="gui.loader.message3"
-            />
-        ),
-        weight: 50
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Loading extensions …"
-                description="One of the loading messages"
-                id="gui.loader.message4"
-            />
-        ),
-        weight: 50
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Creating blocks …"
-                description="One of the loading messages"
-                id="gui.loader.message1"
-            />
-        ),
-        weight: 20
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Herding cats …"
-                description="One of the loading messages"
-                id="gui.loader.message5"
-            />
-        ),
-        weight: 1
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Transmitting nanos …"
-                description="One of the loading messages"
-                id="gui.loader.message6"
-            />
-        ),
-        weight: 1
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Inflating gobos …"
-                description="One of the loading messages"
-                id="gui.loader.message7"
-            />
-        ),
-        weight: 1
-    },
-    {
-        message: (
-            <FormattedMessage
-                defaultMessage="Preparing emojis …"
-                description="One of the loading messages"
-                id="gui.loader.message8"
-            />
-        ),
-        weight: 1
-    }
-];
+
 const mainMessages = {
     'gui.loader.headline': (
         <FormattedMessage
@@ -117,34 +26,6 @@ const mainMessages = {
 };
 
 class LoaderComponent extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            messageNumber: this.chooseRandomMessage()
-        };
-    }
-    componentDidMount () {
-        // Start an interval to choose a new message every 5 seconds
-        this.intervalId = setInterval(() => {
-            this.setState({messageNumber: this.chooseRandomMessage()});
-        }, 5000);
-    }
-    componentWillUnmount () {
-        clearInterval(this.intervalId);
-    }
-    chooseRandomMessage () {
-        let messageNumber;
-        const sum = messages.reduce((acc, m) => acc + m.weight, 0);
-        let rand = sum * Math.random();
-        for (let i = 0; i < messages.length; i++) {
-            rand -= messages[i].weight;
-            if (rand <= 0) {
-                messageNumber = i;
-                break;
-            }
-        }
-        return messageNumber;
-    }
     render () {
         return (
             <div
@@ -173,16 +54,13 @@ class LoaderComponent extends React.Component {
                     <div className={styles.messageContainerOuter}>
                         <div
                             className={styles.messageContainerInner}
-                            style={{transform: `translate(0, -${this.state.messageNumber * 25}px)`}}
+                            id="tw-loading-message"
                         >
-                            {messages.map((m, i) => (
-                                <div
-                                    className={styles.message}
-                                    key={i}
-                                >
-                                    {m.message}
-                                </div>
-                            ))}
+                            <FormattedMessage
+                                defaultMessage="Loading project …"
+                                description="Default loading message"
+                                id="tw.loadingMessage"
+                            />
                         </div>
                     </div>
                 </div>
--- src/playground/tw-hacky-progress-bar.js
+++ src/playground/tw-hacky-progress-bar.js
@@ -3,45 +3,62 @@ const isSupported = () =>
     // If these don't exist Scratch 3 can still load projects but we won't be able to monitor it.
     !!window.Worker && !!window.fetch;
 
-const HIDE_AFTER = 5000;
-
 if (isSupported()) {
     let total = 0;
     let complete = 0;
-    let hideTimeout;
 
-    const progressEl = document.createElement('div');
-    progressEl.style.position = 'absolute';
-    progressEl.style.top = '0';
-    progressEl.style.left = '0';
-    progressEl.style.height = '5px';
-    progressEl.style.backgroundColor = 'white';
-    progressEl.style.zIndex = '99999999';
-    progressEl.style.width = '0';
-    progressEl.style.opacity = '0';
-    progressEl.style.transition = 'all .2s';
-    progressEl.style.pointerEvents = 'none';
-    document.body.appendChild(progressEl);
+    const barOuter = document.createElement('div');
+    barOuter.id = 'tw-progress-bar';
+    barOuter.style.width = '100%';
+    barOuter.style.height = '5px';
+    barOuter.style.border = '1px solid white';
 
-    const hideProgress = () => {
-        progressEl.style.opacity = '0';
-        clearTimeout(hideTimeout);
+    const barInner = document.createElement('div');
+    barInner.style.height = '100%';
+    barInner.style.width = '0';
+    barInner.style.background = 'white';
+    barOuter.appendChild(barInner);
+
+    // 0 - none
+    // 1 - load json
+    // 2 - load assets
+    let state = 0;
+
+    let loadingMessage;
+
+    const setState = newState => {
+        if (state === newState) {
+            return;
+        }
+        state = newState;
+        addBarToDocument();
+        mutationObserver.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
     };
 
-    const showProgress = () => {
-        progressEl.style.opacity = '1';
-        clearTimeout(hideTimeout);
+    const updateMessage = () => {
+        if (loadingMessage) {
+            if (state === 1) {
+                loadingMessage.textContent = `Loading project data …`;
+            } else if (state === 2) {
+                loadingMessage.textContent = `Loading assets (${complete}/${total}) …`;
+            }
+        }
     };
 
     const setProgress = progress => {
-        progressEl.style.width = `${10 + (progress * 90)}%`;
-        if (progress >= 1) {
-            hideProgress();
-        } else {
-            showProgress();
+        if (progress < 0) progress = 0;
+        if (progress > 1) progress = 1;
+        if (progress === 1) {
+            mutationObserver.disconnect();
+            state = 0;
         }
-        clearTimeout(hideTimeout);
-        hideTimeout = setTimeout(hideProgress, HIDE_AFTER);
+        if (state === 2) {
+            updateMessage();
+        }
+        barInner.style.width = `${progress * 100}%`;
     };
 
     const handlePostMessage = e => {
@@ -60,6 +77,7 @@ if (isSupported()) {
         if (isProjectDataRequest(url, opts)) {
             // `url` is known to be a string now
 
+            setState(1);
             setProgress(0);
 
             return new Promise((resolve, reject) => {
@@ -72,6 +90,7 @@ if (isSupported()) {
                         statusText: xhr.statusText
                     }));
                 };
+                xhr.onloadend = () => setProgress(1);
                 xhr.onerror = () => reject(new Error('(tw-hacky-progress-bar) xhr failed'));
                 xhr.onprogress = e => {
                     if (e.lengthComputable) {
@@ -99,6 +118,7 @@ if (isSupported()) {
             // check that this is the right object
             if (message && typeof message === 'object') {
                 if ('url' in message && 'id' in message && 'options' in message) {
+                    setState(2);
                     setProgress(0);
                     total++;
                 }
@@ -113,4 +133,19 @@ if (isSupported()) {
             setProgress(0);
         }
     });
+
+    const addBarToDocument = () => {
+        if (document.getElementById('tw-progress-bar')) {
+            return;
+        }
+
+        const message = document.getElementById('tw-loading-message');
+        if (message) {
+            loadingMessage = message;
+            updateMessage();
+            message.parentElement.parentElement.appendChild(barOuter);
+        }
+    };
+
+    const mutationObserver = new MutationObserver(addBarToDocument);
 }

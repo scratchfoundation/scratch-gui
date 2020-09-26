a57f21efbb9980d4afebfdfc22af6781cea49917
Thomas Weber <weber_t@mailbox.org>
2020-09-25 20:09:21 -0500

    Support ANNOUNCEMENT env var for announcements

--- src/components/tw-home/title.jsx
+++ src/components/tw-home/title.jsx
@@ -5,35 +5,40 @@ import styles from './title.css';
 
 const About = () => {
     return (
-        <div className={styles.title}>
-            <h1>TurboWarp</h1>
-            <span className={styles.subtitles}>
-                <div>
-                    <FormattedMessage
-                        defaultMessage="Unofficial Scratch Mod"
-                        description="Text for explaining this is an unofficial mod of scratch on the home page"
-                        id="tw.home.mod"
-                    />
-                </div>
-                <div>
-                    <a href="https://github.com/TurboWarp/">
+        <React.Fragment>
+            <div className={styles.title}>
+                <h1>TurboWarp</h1>
+                <span className={styles.subtitles}>
+                    <div>
                         <FormattedMessage
-                            defaultMessage="Source Code"
-                            description="Text for source code link on the home page"
-                            id="tw.home.code"
+                            defaultMessage="Unofficial Scratch Mod"
+                            description="Text for explaining this is an unofficial mod of scratch on the home page"
+                            id="tw.home.mod"
                         />
-                    </a>
-                    &nbsp;-&nbsp;
-                    <a href="/privacy.html">
-                        <FormattedMessage
-                            defaultMessage="Privacy"
-                            description="Text for privacy policy link on the home page"
-                            id="tw.home.privacy"
-                        />
-                    </a>
-                </div>
-            </span>
-        </div>
+                    </div>
+                    <div>
+                        <a href="https://github.com/TurboWarp/">
+                            <FormattedMessage
+                                defaultMessage="Source Code"
+                                description="Text for source code link on the home page"
+                                id="tw.home.code"
+                            />
+                        </a>
+                        &nbsp;-&nbsp;
+                        <a href="/privacy.html">
+                            <FormattedMessage
+                                defaultMessage="Privacy"
+                                description="Text for privacy policy link on the home page"
+                                id="tw.home.privacy"
+                            />
+                        </a>
+                    </div>
+                </span>
+            </div>
+            {process.env.ANNOUNCEMENT ? (
+                <p>{process.env.ANNOUNCEMENT}</p>
+            ) : null}
+        </React.Fragment>
     );
 };
 

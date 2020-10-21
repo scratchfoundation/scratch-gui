093c3249709de191780336e5de5c56493a6a5c68
Thomas Weber <weber_t@mailbox.org>
2020-10-21 15:00:00 -0500

    Fix GUI being constantly remounted
    Doing it how it was done previously had the effect of unmounting and remounting the GUI when entering editor
    Fixes project title being reset when opening editor

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -30,117 +30,120 @@ const Interface = ({
     description,
     isFullScreen,
     isPlayerOnly
-}) => (
-    (isPlayerOnly && !isFullScreen) ? (
-        <div className={classNames(styles.container, styles.stageOnly)}>
-            <div className={styles.menu}>
-                <MenuBar
-                    canManageFiles
-                    canChangeLanguage
-                    enableSeeInside
-                />
-            </div>
+}) => {
+    const isHomepage = isPlayerOnly && !isFullScreen;
+    return (
+        <div className={classNames(styles.container, {[styles.stageOnly]: isHomepage})}>
+            {isHomepage ? (
+                <div className={styles.menu}>
+                    <MenuBar
+                        canManageFiles
+                        canChangeLanguage
+                        enableSeeInside
+                    />
+                </div>
+            ) : null}
             <div className={styles.center}>
                 <GUI />
-                <div className={styles.section}>
-                    <ProjectInput />
-                </div>
-                {description.instructions || description.credits ? (
-                    <div className={styles.section}>
-                        <Description
-                            instructions={description.instructions}
-                            credits={description.credits}
-                        />
-                    </div>
+                {isHomepage ? (
+                    <React.Fragment>
+                        <div className={styles.section}>
+                            <ProjectInput />
+                        </div>
+                        {description.instructions || description.credits ? (
+                            <div className={styles.section}>
+                                <Description
+                                    instructions={description.instructions}
+                                    credits={description.credits}
+                                />
+                            </div>
+                        ) : null}
+                        <div className={styles.section}>
+                            <p>
+                                <FormattedMessage
+                                    defaultMessage="TurboWarp is a Scratch mod that compiles projects to JavaScript to make them run really fast. Try it out by inputting a project ID or URL above or choosing an example project below."
+                                    description="Description of TurboWarp"
+                                    id="tw.home.description"
+                                />
+                            </p>
+                        </div>
+                        <div className={styles.section}>
+                            <Examples />
+                        </div>
+                        <footer className={classNames(styles.section, styles.footer)}>
+                            <p>
+                                <FormattedMessage
+                                    defaultMessage="Projects from the Scratch website are licensed under the Creative Commons Attribution-ShareAlike 2.0 license. TurboWarp is not affiliated with Scratch, the Scratch Team, or the Scratch Foundation."
+                                    description="Disclaimer that TurboWarp is not connected to Scratch and licensing information"
+                                    id="tw.footer.disclaimer"
+                                />
+                            </p>
+                            <p>
+                                <FormattedMessage
+                                    defaultMessage="Hosting for TurboWarp is provided by {fosshost}."
+                                    description="Host credit"
+                                    id="tw.footer.host"
+                                    values={{
+                                        fosshost: (
+                                            <a
+                                                href="https://fosshost.org"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                <FormattedMessage
+                                                    defaultMessage="fosshost.org"
+                                                    description="Link to fosshost.org"
+                                                    id="tw.footer.host.fosshost"
+                                                />
+                                            </a>
+                                        )
+                                    }}
+                                />
+                            </p>
+                            <p className={styles.links}>
+                                <a
+                                    href="https://github.com/TurboWarp"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FormattedMessage
+                                        defaultMessage="Source Code"
+                                        description="Link to footer to source code"
+                                        id="tw.footer.source"
+                                    />
+                                </a>
+                                {' - '}
+                                <a
+                                    href="https://scratch.mit.edu/users/GarboMuffin/#comments"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FormattedMessage
+                                        defaultMessage="Feedback & Bugs"
+                                        description="Link in footer to give feedback"
+                                        id="tw.footer.feedback"
+                                    />
+                                </a>
+                                {' - '}
+                                <a
+                                    href="privacy.html"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <FormattedMessage
+                                        defaultMessage="Privacy"
+                                        description="Link in footer to privacy policy"
+                                        id="tw.footer.privacy"
+                                    />
+                                </a>
+                            </p>
+                        </footer>
+                    </React.Fragment>
                 ) : null}
-                <div className={styles.section}>
-                    <p>
-                        <FormattedMessage
-                            defaultMessage="TurboWarp is a Scratch mod that compiles projects to JavaScript to make them run really fast. Try it out by inputting a project ID or URL above or choosing an example project below."
-                            description="Description of TurboWarp"
-                            id="tw.home.description"
-                        />
-                    </p>
-                </div>
-                <div className={styles.section}>
-                    <Examples />
-                </div>
-                <footer className={classNames(styles.section, styles.footer)}>
-                    <p>
-                        <FormattedMessage
-                            defaultMessage="Projects from the Scratch website are licensed under the Creative Commons Attribution-ShareAlike 2.0 license. TurboWarp is not affiliated with Scratch, the Scratch Team, or the Scratch Foundation."
-                            description="Disclaimer that TurboWarp is not connected to Scratch and licensing information"
-                            id="tw.footer.disclaimer"
-                        />
-                    </p>
-                    <p>
-                        <FormattedMessage
-                            defaultMessage="Hosting for TurboWarp is provided by {fosshost}."
-                            description="Host credit"
-                            id="tw.footer.host"
-                            values={{
-                                fosshost: (
-                                    <a
-                                        href="https://fosshost.org"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        <FormattedMessage
-                                            defaultMessage="fosshost.org"
-                                            description="Link to fosshost.org"
-                                            id="tw.footer.host.fosshost"
-                                        />
-                                    </a>
-                                )
-                            }}
-                        />
-                    </p>
-                    <p className={styles.links}>
-                        <a
-                            href="https://github.com/TurboWarp"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FormattedMessage
-                                defaultMessage="Source Code"
-                                description="Link to footer to source code"
-                                id="tw.footer.source"
-                            />
-                        </a>
-                        {' - '}
-                        <a
-                            href="https://scratch.mit.edu/users/GarboMuffin/#comments"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FormattedMessage
-                                defaultMessage="Feedback & Bugs"
-                                description="Link in footer to give feedback"
-                                id="tw.footer.feedback"
-                            />
-                        </a>
-                        {' - '}
-                        <a
-                            href="privacy.html"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FormattedMessage
-                                defaultMessage="Privacy"
-                                description="Link in footer to privacy policy"
-                                id="tw.footer.privacy"
-                            />
-                        </a>
-                    </p>
-                </footer>
             </div>
         </div>
-    ) : (
-        <div className={styles.container}>
-            <GUI />
-        </div>
-    )
-);
+    );
+};
 
 Interface.propTypes = {
     description: PropTypes.shape({

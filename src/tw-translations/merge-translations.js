bef2ef3e98ff6176522fce6290a8877a09954266
Thomas Weber <weber_t@mailbox.org>
2020-11-10 21:10:36 -0600

    Update to handle chanes in TurboWarp/translations

--- src/tw-translations/merge-translations.js
+++ src/tw-translations/merge-translations.js
@@ -2,6 +2,9 @@ import translations from './translations.json';
 
 const mergeMessages = messages => {
     for (const language of Object.keys(translations)) {
+        if (language.startsWith('_')) {
+            continue;
+        }
         const languageMessages = messages[language];
         const newMessages = translations[language];
         for (const messageId of Object.keys(newMessages)) {

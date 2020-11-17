b23350e0b36099990a9f720065a3566143571a82
Thomas Weber <weber_t@mailbox.org>
2020-11-17 13:32:04 -0600

    Put a single "is compiled?" in the toolbox
    The main issue with is compiled? returning 0 on its own is now fixed.

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -727,19 +727,8 @@ const myBlocks = function () {
 const turbowarp = function () {
     return `
     <category name="TurboWarp" id="turbowarp" colour="#FF4D4D" secondaryColour="#FF3D3D">
-        <block type="control_if">
-            <value name="CONDITION">
-                <block type="argument_reporter_boolean">
-                    <field name="VALUE">is compiled?</field>
-                </block>
-            </value>
-        </block>
-        <block type="control_if_else">
-            <value name="CONDITION">
-                <block type="argument_reporter_boolean">
-                    <field name="VALUE">is compiled?</field>
-                </block>
-            </value>
+        <block type="argument_reporter_boolean">
+            <field name="VALUE">is compiled?</field>
         </block>
         <block type="argument_reporter_string_number">
             <field name="VALUE">last key pressed</field>

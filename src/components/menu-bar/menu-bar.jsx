41efe03067eef05f887d0b7dcd4469308c75a1e0
Thomas Weber <weber_t@mailbox.org>
2020-11-26 13:36:31 -0600

    "mode" in "60 FPS mode" should be capitalized

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -593,13 +593,13 @@ class MenuBar extends React.Component {
                                         <MenuItem onClick={toggleSixtyFPS}>
                                             {isSixty ? (
                                                 <FormattedMessage
-                                                    defaultMessage="Turn off 60 FPS mode"
+                                                    defaultMessage="Turn off 60 FPS Mode"
                                                     description="Menu bar item for turning off 60 FPS mode"
                                                     id="tw.menuBar.60off"
                                                 />
                                             ) : (
                                                 <FormattedMessage
-                                                    defaultMessage="Turn on 60 FPS mode"
+                                                    defaultMessage="Turn on 60 FPS Mode"
                                                     description="Menu bar item for turning on 60 FPS mode"
                                                     id="tw.menuBar.60on"
                                                 />

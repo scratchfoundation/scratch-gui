cc4cac20ba8fe497c800ae02e7891714a72f35cb
Thomas Weber <weber_t@mailbox.org>
2020-12-05 23:17:03 -0600

    Fix file type issues

--- src/lib/tw-filesystem-api.js
+++ src/lib/tw-filesystem-api.js
@@ -1,34 +1,27 @@
 const available = () => !!window.showSaveFilePicker;
 
-const scratch1 = {
-    description: 'Scratch 1 Project',
-    accept: {
-        // TODO: not sure what the proper type for this is
-        'application/x.scratch.sb': '.sb'
-    }
-};
-const scratch2 = {
-    description: 'Scratch 2 Project',
-    accept: {
-        // TODO: not sure what the proper type for this is
-        'application/x.scratch.sb2': '.sb2'
-    }
-};
-const scratch3 = {
-    description: 'Scratch 3 Project',
-    accept: {
-        'application/x.scratch.sb3': '.sb3'
-    }
-};
-
 const showSaveFilePicker = async () => await window.showSaveFilePicker({
-    types: [scratch3]
+    types: [
+        {
+            description: 'Scratch 3 Project',
+            accept: {
+                'application/x.scratch.sb3': '.sb3'
+            }
+        }
+    ]
 });
 
 const showOpenFilePicker = async () => {
     const [handle] = await window.showOpenFilePicker({
         multiple: false,
-        types: [scratch3, scratch2, scratch1]
+        types: [
+            {
+                description: 'Scratch Project',
+                accept: {
+                    'application/x.scratch.sb3': ['.sb', '.sb2', '.sb3']
+                }
+            }
+        ]
     });
     return handle;
 };

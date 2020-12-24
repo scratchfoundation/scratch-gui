09a4ab1772da97594323a9355d916edec5babe30
Thomas Weber <weber_t@mailbox.org>
2020-12-24 17:19:52 -0600

    Update addon IDs
    Make them match their SA ID

--- src/addons/index.js
+++ src/addons/index.js
@@ -1,13 +1,10 @@
 import API from './api';
-
-/* eslint-disable import/no-commonjs */
-const addons = [
-    'searchable-dropdowns',
-    'dev-tools',
-    'block-switching'
-];
+import addons from './addons.json';
 
 for (const addonId of addons) {
+    if (addonId.startsWith('//')) {
+        continue;
+    }
     const api = new API(addonId);
     const module = require(`./addons/${addonId}`);
     module.default(api);

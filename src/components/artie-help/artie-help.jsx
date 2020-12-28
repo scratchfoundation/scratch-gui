bfdfd50424cc36880306056f621f94f9a5c12961
Luis Eduardo Imbernón <leimbernon@ai-mind.com>
2020-12-28 21:21:31 +0100

    Deleted Unused elements

--- src/components/artie-help/artie-help.jsx
+++ src/components/artie-help/artie-help.jsx
@@ -2,15 +2,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Box from '../box/box.jsx';
 import Modal from '../../containers/modal.jsx';
-import {getBlocksFromHelp} from '../../lib/artie-api';
-import cloneDeep from 'lodash/cloneDeep';
 
 import styles from '../custom-procedures/custom-procedures.css';
-import textInputIcon from '../custom-procedures/icon--text-input.svg';
-import booleanInputIcon from '../custom-procedures/icon--boolean-input.svg';
-import labelIcon from '../custom-procedures/icon--label.svg';
-import VMScratchBlocks from '../../lib/blocks';
-import {defineMessages, injectIntl, intlShape, FormattedMessage} from 'react-intl';
+import {defineMessages} from 'react-intl';
 
 const messages = defineMessages({
     myblockModalTitle: {
@@ -24,16 +18,6 @@ class ArtieHelpComponent extends React.Component {
 
     constructor(props){
         super(props);
-    }
-
-    componentDidMount(){
-        //var objBlocks = getBlocksFromHelp(this.props.vm.runtime.flyoutBlocks._blocks, this.props.help);
-
-        //We clone the VM for the blocks we have to add
-        //let localAddVm = cloneDeep(this.props.vm);
-
-        //We clone the VM for the blocks we have to delete
-        //let localDeleteVm = cloneDeep(this.props.vm);
     }
 
     render(){

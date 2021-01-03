408cfcf9e5259f3e6188de9888a3c6a24b313d60
Thomas Weber <weber_t@mailbox.org>
2021-01-03 00:55:19 -0600

    Bring AUTOSAVE_TIMEOUT back up to 1 min

--- src/lib/tw-autosave-hoc.jsx
+++ src/lib/tw-autosave-hoc.jsx
@@ -9,7 +9,7 @@ import VM from 'scratch-vm';
 import AutoSaveAPI from './tw-indexeddb-autosave-api';
 
 // TODO: increase this
-const AUTOSAVE_TIMEOUT = 1000 * 60 * 0.1;
+const AUTOSAVE_TIMEOUT = 1000 * 60 * 1;
 
 let bailed = false;
 

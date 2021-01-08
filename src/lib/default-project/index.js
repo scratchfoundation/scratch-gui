21854b03116edfb8f554971d75463c88f0bb8511
DD Liu <liudi@media.mit.edu>
2021-01-08 11:39:43 -0500

    Merge release 1/7/2021

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -4,8 +4,8 @@ import projectData from './project-data';
 import popWav from '!arraybuffer-loader!./83a9787d4cb6f3b7632b4ddfebf74367.wav';
 import meowWav from '!arraybuffer-loader!./83c36d806dc92327b9e7049a565c6bff.wav';
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
-import costume1 from '!raw-loader!./b7853f557e4426412e64bb3da6531a99.svg';
-import costume2 from '!raw-loader!./e6ddc55a6ddd9cc9d84fe0b4c21e016f.svg';
+import costume1 from '!raw-loader!./bcf454acf82e4504149f7ffe07081dbc.svg';
+import costume2 from '!raw-loader!./0fb9be3e8397c983338cb71dc84d0b25.svg';
 /* eslint-enable import/no-unresolved */
 
 const defaultProject = translator => {
@@ -40,12 +40,12 @@ const defaultProject = translator => {
         dataFormat: 'SVG',
         data: encoder.encode(backdrop)
     }, {
-        id: 'b7853f557e4426412e64bb3da6531a99',
+        id: 'bcf454acf82e4504149f7ffe07081dbc',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume1)
     }, {
-        id: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f',
+        id: '0fb9be3e8397c983338cb71dc84d0b25',
         assetType: 'ImageVector',
         dataFormat: 'SVG',
         data: encoder.encode(costume2)

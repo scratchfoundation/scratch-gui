592104c87e43465d58712a62dc01f5c990b4e2af
Thomas Weber <weber_t@mailbox.org>
2021-01-19 14:02:22 -0600

    Remove clipboardWrite addons when clipboard API is not supported

--- src/addons/addon-manifests.js
+++ src/addons/addon-manifests.js
@@ -1,8 +1,15 @@
 import addons from './addons.json';
 
+const clipboardSupported = !!navigator.clipboard;
+
 const addonMap = {};
 for (const addonId of addons) {
     const manifest = require(`./addons/${addonId}/addon.json`);
+    if (manifest.permissions) {
+        if (!clipboardSupported && manifest.permissions.includes('clipboardWrite')) {
+            continue;
+        }
+    }
     addonMap[addonId] = manifest;
 }
 

c5e075e8c2dcf4deabc0badd1c27e8b3aae8b853
Thomas Weber <weber_t@mailbox.org>
2021-01-22 16:52:48 -0600

    Fix toggle button styles

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -411,6 +411,15 @@ class MenuBar extends React.Component {
                             </div>
                             <LanguageSelector label={this.props.intl.formatMessage(ariaMessages.language)} />
                         </div>)}
+                        {/* tw: theme toggler */}
+                        {this.props.onClickTheme && (
+                            <div
+                                className={classNames(styles.menuBarItem, styles.hoverable)}
+                                onMouseUp={this.props.onClickTheme}
+                            >
+                                <div className={styles.themeIcon} />
+                            </div>
+                        )}
                         {/* tw: display compile errors */}
                         {this.props.compileErrors.length > 0 && <div>
                             <div
@@ -464,12 +473,6 @@ class MenuBar extends React.Component {
                                 </MenuBarMenu>
                             </div>
                         </div>}
-                        {this.props.onClickTheme && (
-                            <div
-                                className={classNames(styles.menuBarItem, styles.hoverable, styles.themeButton)}
-                                onMouseUp={this.props.onClickTheme}
-                            />
-                        )}
                         {(this.props.canManageFiles) && (
                             <div
                                 className={classNames(styles.menuBarItem, styles.hoverable, {

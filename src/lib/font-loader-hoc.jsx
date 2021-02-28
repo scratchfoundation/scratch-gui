68551ecd5ae1c2f443c1cd4a12220cf87653c0c6
Thomas Weber <weber_t@mailbox.org>
2021-02-27 22:27:32 -0600

    New font loading method

--- src/lib/font-loader-hoc.jsx
+++ src/lib/font-loader-hoc.jsx
@@ -3,18 +3,8 @@ import PropTypes from 'prop-types';
 import omit from 'lodash.omit';
 import {connect} from 'react-redux';
 import {setFontsLoaded} from '../reducers/fonts-loaded';
+import {loadFonts} from 'scratch-render-fonts';
 
-// This list is from scratch-render-fonts:
-// https://github.com/LLK/scratch-render-fonts/blob/master/src/index.js#L4
-const FONTS = [
-    'Sans Serif',
-    'Serif',
-    'Handwriting',
-    'Marker',
-    'Curly',
-    'Pixel',
-    'Scratch'
-];
 /* Higher Order Component to provide behavior for loading fonts.
  * @param {React.Component} WrappedComponent component to receive fontsLoaded prop
  * @returns {React.Component} component with font loading behavior
@@ -24,40 +14,7 @@ const FontLoaderHOC = function (WrappedComponent) {
         componentDidMount () {
             if (this.props.fontsLoaded) return;
 
-            const getFontPromises = () => {
-                const fontPromises = [];
-                // Browsers that support the font loader interface have an iterable document.fonts.values()
-                // Firefox has a mocked out object that doesn't actually implement iterable, which is why
-                // the deep safety check is necessary.
-                if (document.fonts &&
-                    typeof document.fonts.values === 'function' &&
-                    typeof document.fonts.values()[Symbol.iterator] === 'function') {
-                    for (const fontFace of document.fonts.values()) {
-                        // Only load fonts from this list. If we load all fonts on the document, we may block on
-                        // loading fonts from things like chrome extensions.
-                        if (FONTS.indexOf(fontFace.family) !== -1) {
-                            fontPromises.push(fontFace.loaded);
-                            fontFace.load();
-                        }
-                    }
-                }
-                return fontPromises;
-            };
-            // Font promises must be gathered after the document is loaded, because on Mac Chrome, the promise
-            // objects get replaced and the old ones never resolve.
-            if (document.readyState === 'complete') {
-                Promise.all(getFontPromises()).then(() => {
-                    this.props.onSetFontsLoaded();
-                });
-            } else {
-                document.onreadystatechange = () => {
-                    if (document.readyState !== 'complete') return;
-                    document.onreadystatechange = null;
-                    Promise.all(getFontPromises()).then(() => {
-                        this.props.onSetFontsLoaded();
-                    });
-                };
-            }
+            loadFonts().then(() => this.props.onSetFontsLoaded());
         }
         render () {
             const componentProps = omit(this.props, ['onSetFontsLoaded']);
--- src/lib/tw-scratch-render-fonts/index.js
+++ src/lib/tw-scratch-render-fonts/index.js
@@ -5,8 +5,9 @@ import Marker from './Knewave.ttf';
 import Curly from './Griffy-Regular.ttf';
 import Pixel from './Grand9K-Pixel.ttf';
 import Scratch from './Scratch.ttf';
+import log from '../log';
 
-const FONTS = {
+const fontSource = {
     'Sans Serif': SansSerif,
     'Serif': Serif,
     'Handwriting': Handwriting,
@@ -16,20 +17,70 @@ const FONTS = {
     'Scratch': Scratch
 };
 
-for (const fontName in FONTS) {
-    const fontSource = FONTS[fontName];
-    FONTS[fontName] = `@font-face { font-family: "${fontName}"; src: url("${fontSource}"); }`;
-}
+const fontData = {};
 
-if (!document.getElementById('scratch-font-styles')) {
+const fetchFonts = () => {
+    const promises = [];
+    for (const fontName of Object.keys(fontSource)) {
+        promises.push(fetch(fontSource[fontName])
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Cannot load font: ${fontName} (invalid HTTP response)`);
+                }
+                return res.blob();
+            })
+            .then(blob => new Promise((resolve, reject) => {
+                const fr = new FileReader();
+                fr.onload = () => resolve(fr.result);
+                fr.onerror = () => reject(new Error(`Cannot load font: ${fontName} (could not read)`));
+                fr.readAsDataURL(blob);
+            }))
+            .then(url => {
+                fontData[fontName] = `@font-face{font-family:"${fontName}";src:url("${url}");}`;
+            })
+            .catch(err => {
+                log.error(err);
+            })
+        );
+    }
+    return Promise.all(promises);
+};
+
+const addFontsToDocument = () => {
+    if (document.getElementById('scratch-font-styles')) {
+        return;
+    }
     const documentStyleTag = document.createElement('style');
     documentStyleTag.id = 'scratch-font-styles';
-    for (const fontName in FONTS) {
-        documentStyleTag.textContent += FONTS[fontName];
+    for (const fontName of Object.keys(fontSource)) {
+        const css = fontData[fontName];
+        if (css) {
+            documentStyleTag.textContent += css;
+        }
     }
     document.body.insertBefore(documentStyleTag, document.body.firstChild);
-}
+};
+
+const waitForFontsToLoad = () => {
+    const promises = [];
+    if (document.fonts && document.fonts.load) {
+        for (const fontName in fontData) {
+            promises.push(document.fonts.load(`12px ${fontName}`));
+        }
+    }
+    return Promise.all(promises);
+};
+
+const loadFonts = () => fetchFonts()
+    .then(() => {
+        addFontsToDocument();
+        return waitForFontsToLoad();
+    })
+    .catch(err => {
+        log.error(err);
+    });
 
 export {
-    FONTS
+    loadFonts,
+    fontData as FONTS
 };

d88ae9a82636c446faeed5db60dfee66d907d9ab
Thomas Weber <weber_t@mailbox.org>
2021-02-28 15:10:53 -0600

    Revert "Load addons a little bit later"
    This reverts commit c3b20159c8f81cb2e422171eb549f10deb334492.

--- src/playground/render-interface.jsx
+++ src/playground/render-interface.jsx
@@ -80,13 +80,12 @@ const WrappedMenuBar = compose(
     SBFileUploaderHOC
 )(MenuBar);
 
+import(/* webpackChunkName: "addons" */ '../addons/entry');
+
 class Interface extends React.Component {
     constructor (props) {
         super(props);
         this.handleUpdateProjectTitle = this.handleUpdateProjectTitle.bind(this);
-    }
-    componentDidMount () {
-        import(/* webpackChunkName: "addons" */ '../addons/entry');
     }
     handleUpdateProjectTitle (title, isDefault) {
         if (isDefault || !title) {

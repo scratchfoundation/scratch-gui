daa5e7ea83fdb83bc31a0d6d19a38dd38dc2ce36
Sandy Enoch <alexander@robotical.io>
2021-03-08 01:11:54 +0000

    Update default project to remove sounds, remove duplicate marty sprite costume, make Marty sprite display size smaller

--- src/lib/default-project/project-data.js
+++ src/lib/default-project/project-data.js
@@ -52,15 +52,6 @@ const projectData = translateFunction => {
                     }
                 ],
                 sounds: [
-                    {
-                        assetId: '83a9787d4cb6f3b7632b4ddfebf74367',
-                        name: translator(messages.pop),
-                        dataFormat: 'wav',
-                        format: '',
-                        rate: 11025,
-                        sampleCount: 258,
-                        md5ext: '83a9787d4cb6f3b7632b4ddfebf74367.wav'
-                    }
                 ],
                 volume: 100
             },
@@ -81,27 +72,9 @@ const projectData = translateFunction => {
                         dataFormat: 'svg',
                         rotationCenterX: 48,
                         rotationCenterY: 50
-                    },
-                    {
-                        assetId: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f',
-                        name: translator(messages.costume, {index: 2}),
-                        bitmapResolution: 1,
-                        md5ext: 'e6ddc55a6ddd9cc9d84fe0b4c21e016f.svg',
-                        dataFormat: 'svg',
-                        rotationCenterX: 46,
-                        rotationCenterY: 53
                     }
                 ],
                 sounds: [
-                    {
-                        assetId: '83c36d806dc92327b9e7049a565c6bff',
-                        name: translator(messages.meow),
-                        dataFormat: 'wav',
-                        format: '',
-                        rate: 22050,
-                        sampleCount: 18688,
-                        md5ext: '83c36d806dc92327b9e7049a565c6bff.wav'
-                    }
                 ],
                 volume: 100,
                 visible: true,

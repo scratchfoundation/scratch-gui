9a628f672b5a9d6094113c6c093b449eea5f3b6d
Thomas Weber <weber_t@mailbox.org>
2021-03-21 18:09:20 -0500

    Don't leak onHasCloudVariablesChanged

--- src/lib/vm-listener-hoc.jsx
+++ src/lib/vm-listener-hoc.jsx
@@ -196,6 +196,7 @@ const vmListenerHOC = function (WrappedComponent) {
                 onRuntimeStopped,
                 onTurboModeOff,
                 onTurboModeOn,
+                onHasCloudVariablesChanged,
                 onFramerateChanged,
                 onInterpolationChanged,
                 onCompilerOptionsChanged,

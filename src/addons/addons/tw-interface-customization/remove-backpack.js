c1a797b5a20e689a941e16f3ac7e9abd9080a00c
Thomas Weber <weber_t@mailbox.org>
2021-03-21 17:18:18 -0500

    Update addons

--- src/addons/addons/tw-interface-customization/remove-backpack.js
+++ src/addons/addons/tw-interface-customization/remove-backpack.js
@@ -4,5 +4,6 @@
  */
 
 export default async function () {
+  // Trigger a resize so that Blockly knows that the backpack is gone
   window.dispatchEvent(new Event("resize"));
 }
---
+++ src/addons/addons/tw-interface-customization/remove-warning.js
@@ -0,0 +1,19 @@
+/**!
+ * Imported from SA
+ * @license GPLv3.0 (see LICENSE_GPL or https://www.gnu.org/licenses/ for more information)
+ */
+
+export default async function ({ addon }) {
+  addon.tab.redux.initialize();
+  const close = () =>
+    addon.tab.redux.dispatch({
+      type: "scratch-gui/alerts/CLOSE_ALERTS_WITH_ID",
+      alertId: "twWarning",
+    });
+  addon.tab.redux.addEventListener("statechanged", ({ detail }) => {
+    if (detail.action.type === "scratch-gui/alerts/SHOW_ALERT" && detail.action.alertId === "twWarning") {
+      (window.queueMicrotask || window.setTimeout)(close);
+    }
+  });
+  close();
+}

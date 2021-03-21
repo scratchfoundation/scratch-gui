dde721dbe8ce9c7b3e320ad3cce116925dd2d2f9
Thomas Weber <weber_t@mailbox.org>
2021-03-20 21:04:49 -0500

    Get rid of that warning about isrtl

--- src/containers/target-pane.jsx
+++ src/containers/target-pane.jsx
@@ -244,6 +244,7 @@ class TargetPane extends React.Component {
             onReceivedBlocks,
             onShowImporting,
             workspaceMetrics,
+            isRtl,
             ...componentProps
         } = this.props;
         /* eslint-enable no-unused-vars */

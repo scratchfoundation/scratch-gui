6e6b392f5f87440f2fa9de7d429f21fa3e25f4a4
Thomas Weber <weber_t@mailbox.org>
2021-03-25 20:45:53 -0500

    Log some information to the console about how to access and use the VM

--- src/lib/vm-manager-hoc.jsx
+++ src/lib/vm-manager-hoc.jsx
@@ -37,6 +37,10 @@ const vmManagerHOC = function (WrappedComponent) {
                 this.props.vm.setLocale(this.props.locale, this.props.messages);
                 // tw: load our extension automatically
                 this.props.vm.extensionManager.loadExtensionIdSync('tw');
+                /* eslint-disable no-console */
+                console.log('The VM is exposed as `vm`');
+                console.log('To log compiled scripts to the console, run `vm.enableDebug()`');
+                /* eslint-enable no-console */
             }
             if (!this.props.isPlayerOnly && !this.props.isStarted) {
                 this.props.vm.start();

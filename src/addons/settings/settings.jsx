43e93cb115025c2a9ad002704e98acfcc3783f32
Thomas Weber <weber_t@mailbox.org>
2021-04-03 20:06:47 -0500

    Make everything "GPL 3.0 only"
    Some files had headers that stated they were "GPL 3.0 or later"
    I've decided to just make everything consistent as "GPL 3.0 only"
    The primary reason for this is that SA appears to be impled "GPL 3.0 only" and I'm not sure if mixing "GPL 3.0 only" and "GPL 3.0 or later" is a thing you can do
    As the sole copyright owner, relicensing is not a problem.

--- src/addons/settings/settings.jsx
+++ src/addons/settings/settings.jsx
@@ -1,10 +1,9 @@
 /**
- * Copyright (c) 2021 Thomas Weber
+ * Copyright (C) 2021 Thomas Weber
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of

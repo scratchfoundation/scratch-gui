6033f09b6e3f860bdca5e51b453c97df416c3c5e
Thomas Weber <weber_t@mailbox.org>
2021-04-04 19:06:41 -0500

    Fix project_url in embed

--- src/playground/embed.jsx
+++ src/playground/embed.jsx
@@ -24,7 +24,7 @@ const getProjectId = () => {
     if (pathMatch !== null) {
         return pathMatch[pathMatch.length - 1];
     }
-    return null;
+    return '0';
 };
 
 const projectId = getProjectId();

6d94f6a9644741af5b97d93592955bccf58f9569
Thomas Weber <weber_t@mailbox.org>
2021-04-07 13:29:24 -0500

    Update addons

--- src/addons/addons/mute-project/userscript.js
+++ src/addons/addons/mute-project/userscript.js
@@ -12,39 +12,25 @@ export default async function ({ addon, global, console }) {
   icon.loading = "lazy";
   icon.src = _twGetAsset("/icon--mute.svg");
   icon.style.display = "none";
-  let ctrlPressesCount = 0;
-  let ctrlPressedRecently = false;
-
-  window.addEventListener("keydown", (event) => {
-    if (event.ctrlKey) {
-      ctrlPressesCount++;
-      const pressCount = ctrlPressesCount;
-      ctrlPressedRecently = true;
-      setTimeout(() => {
-        if (pressCount === ctrlPressesCount) ctrlPressedRecently = false;
-      }, 2500);
+  const toggleMute = (e) => {
+    if (e.ctrlKey) {
+      e.cancelBubble = true;
+      e.preventDefault();
+      muted = !muted;
+      if (muted) {
+        vm.runtime.audioEngine.inputNode.gain.value = 0;
+        icon.style.display = "block";
+      } else {
+        vm.runtime.audioEngine.inputNode.gain.value = 1;
+        icon.style.display = "none";
+      }
     }
-  });
-
+  };
   while (true) {
     let button = await addon.tab.waitForElement("[class^='green-flag_green-flag']", { markAsSeen: true });
     let container = button.parentElement;
     container.appendChild(icon);
-    const clickListener = (e) => {
-      if (ctrlPressedRecently) {
-        e.cancelBubble = true;
-        e.preventDefault();
-        muted = !muted;
-        if (muted) {
-          vm.runtime.audioEngine.inputNode.gain.value = 0;
-          icon.style.display = "block";
-        } else {
-          vm.runtime.audioEngine.inputNode.gain.value = 1;
-          icon.style.display = "none";
-        }
-      }
-    };
-    button.addEventListener("click", clickListener);
-    button.addEventListener("contextmenu", clickListener);
+    button.addEventListener("click", toggleMute);
+    button.addEventListener("contextmenu", toggleMute);
   }
 }

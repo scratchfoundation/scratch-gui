ed77d4cb8bb73ab59f3657228abd64ce5f466a19
GarboMuffin <weber_t@mailbox.org>
2021-05-01 22:40:46 -0500

    Update addons (enable drag-drop by default)

--- src/addons/addons/drag-drop/userscript.js
+++ src/addons/addons/drag-drop/userscript.js
@@ -24,10 +24,7 @@ export default async function ({ addon, global, console }) {
         fileInput.files = files;
         fileInput.dispatchEvent(new Event("change", { bubbles: true }));
       };
-    } else if (
-      !addon.tab.redux.state.scratchGui.mode.isPlayerOnly &&
-      (el = e.target.closest('div[class*="monitor_list-monitor"]'))
-    ) {
+    } else if ((el = e.target.closest('div[class*="monitor_list-monitor"]'))) {
       callback = (files) => {
         const contextMenuBefore = document.querySelector("body > .react-contextmenu.react-contextmenu--visible");
         // Simulate a right click on the list monitor

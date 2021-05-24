f442d1d0a09be6045902c82f5c79487b2e789fdd
Thomas Weber <weber_t@mailbox.org>
2021-05-24 18:07:59 -0500

    Expose redux on window.ReduxStore

--- src/lib/app-state-hoc.jsx
+++ src/lib/app-state-hoc.jsx
@@ -93,6 +93,7 @@ const AppStateHOC = function (WrappedComponent, localesOnly) {
                 initialState,
                 enhancer
             );
+            window.ReduxStore = this.store;
             AddonHooks.appStateStore = this.store;
         }
         componentDidUpdate (prevProps) {

1070fa9029ce60fc5925719846ab1661297a068f
Thomas Weber <weber_t@mailbox.org>
2021-06-02 21:19:56 -0500

    Remove some items from service worker asset list

--- src/playground/generate-service-worker-plugin.js
+++ src/playground/generate-service-worker-plugin.js
@@ -1,10 +1,9 @@
 /* eslint-disable import/no-commonjs */
 
-const pluginName = 'TWGenerateServiceWorkerPlugin';
 const RawSource = require('webpack-sources').RawSource;
 
+const PLUGIN_NAME = 'TWGenerateServiceWorkerPlugin';
 const SW_NAME = 'sw.js';
-
 const INCLUDE_HTML = [
     'index.html',
     'editor.html',
@@ -14,18 +13,44 @@ const INCLUDE_HTML = [
 
 class TWGenerateServiceWorkerPlugin {
     apply (compiler) {
-        compiler.hooks.emit.tap(pluginName, compilation => {
+        compiler.hooks.emit.tap(PLUGIN_NAME, compilation => {
             const assetNames = compilation.getAssets()
                 .map(i => i.name)
                 .filter(name => {
-                    if (name.endsWith('.map')) return false;
-                    if (name.includes('worker')) return false;
-                    if (name.startsWith('js/embed')) return false;
-                    if (name.startsWith('js/credits')) return false;
-                    if (name.startsWith('js/library-')) return false;
-                    if (name === SW_NAME) return false;
-                    if (name.endsWith('.html') && !INCLUDE_HTML.includes(name)) return false;
-                    return true;
+                    if (name.startsWith('static/blocks-media') || name.startsWith('static/assets')) {
+                        // Assets that are only used in horizontal mode
+                        if (
+                            name.includes('event_broadcast_') ||
+                            name.includes('event_when-broadcast-received_') ||
+                            name.includes('event_whenflagclicked')
+                        ) return false;
+                        // Assets that are useless without an internet connection
+                        if (
+                            name.includes('wedo_') ||
+                            name.includes('set-led_') ||
+                            name.includes('microbit-block-icon') ||
+                            name.includes('wedo2-block-icon')
+                        ) return false;
+                        // Assets that are extremely unlikely to be used
+                        if (
+                            name.endsWith('.wav') ||
+                            name.endsWith('.ogg') ||
+                            name.endsWith('.cur')
+                        ) return false;
+                        return true;
+                    }
+                    if (name.startsWith('js/')) {
+                        // Extension worker
+                        if (name.includes('worker')) return false;
+                        // Unnecessary pages
+                        if (name.startsWith('js/embed')) return false;
+                        if (name.startsWith('js/credits')) return false;
+                        // Features that won't work offline anyways
+                        if (name.startsWith('js/library-')) return false;
+                        return true;
+                    }
+                    if (INCLUDE_HTML.includes(name)) return true;
+                    return false;
                 });
             const workerFile = compilation.getAsset(SW_NAME);
             const workerSource = workerFile.source.source().toString();

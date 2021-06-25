bbfd9e03dd4bb9aefc27669c27b048bba9bd447e
Thomas Weber <weber_t@mailbox.org>
2021-06-25 11:17:56 -0500

    Remove pop sound by default

--- src/lib/empty-assets.js
+++ src/lib/empty-assets.js
@@ -27,16 +27,7 @@ const emptyCostume = name => ({
  */
 const emptySprite = (name, soundName, costumeName) => ({
     objName: name,
-    sounds: [
-        {
-            soundName: soundName,
-            soundID: -1,
-            md5: '83a9787d4cb6f3b7632b4ddfebf74367.wav',
-            sampleCount: 258,
-            rate: 11025,
-            format: ''
-        }
-    ],
+    sounds: [],
     costumes: [
         {
             costumeName: costumeName,

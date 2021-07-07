706e60429a5299d6dd6b0420fbccbca147fee7b1
DD <liudi08@gmail.com>
2021-07-07 18:12:57 +0000

    Build for dfd54dbf59abb65b0ca9fb10b03bc38f7b25004c [skip ci]

--- develop/chunks/compatibilitytesting.js
+++ develop/chunks/compatibilitytesting.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[4],{
 
-/***/ 1581:
+/***/ 1582:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -112,5 +112,5 @@ react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render( /*#__PURE__*/react__WEB
 
 /***/ })
 
-},[[1581,0]]]);
+},[[1582,0]]]);
 //# sourceMappingURL=compatibilitytesting.js.map

3280ec95f67ee91a6e52d0a9b0dfe40ee4d27b9e
Aaron <1581273984@qq.com>
2021-12-11 00:11:59 +0800

    fix: when input of import csv cancel,the input element should be hidden

--- src/lib/import-csv.js
+++ src/lib/import-csv.js
@@ -1,9 +1,11 @@
 import Papa from 'papaparse';
 
 export default () => new Promise((resolve, reject) => {
-    const fileInput = document.createElement('input');
+    const fileInput = document.getElementById('import_csv') || document.createElement('input');
     fileInput.setAttribute('type', 'file');
     fileInput.setAttribute('accept', '.csv, .tsv, .txt'); // parser auto-detects delimiter
+    fileInput.setAttribute('style', 'display: none');
+    fileInput.setAttribute('id', 'import_csv');
     fileInput.onchange = e => {
         const file = e.target.files[0];
         Papa.parse(file, {

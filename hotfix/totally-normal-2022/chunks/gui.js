b66a0b0a4cf87227da4067a76dfd2a0b1cfdab9e
rschamp <rschamp@media.mit.edu>
2022-03-21 20:06:17 +0000

    Build for 6f407f319e047c96c1037da52240ee5d7185b645 [skip ci]

--- hotfix/totally-normal-2022/chunks/gui.js
+++ hotfix/totally-normal-2022/chunks/gui.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[7],{
 
-/***/ 1359:
+/***/ 1360:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(10)(false);
@@ -18,7 +18,7 @@ exports.locals = {
 
 /***/ }),
 
-/***/ 1360:
+/***/ 1361:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -73,6 +73,8 @@ var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
 
   var backpackHostMatches = window.location.href.match(/[?&]backpack_host=([^&]*)&?/);
   var backpackHost = backpackHostMatches ? backpackHostMatches[1] : null;
+  var totallyNormalMatches = window.location.href.match(/[?&]totallyNormal=([^&]*)&?/);
+  var isTotallyNormal = totallyNormalMatches ? totallyNormalMatches[1] === 'true' : false;
   var scratchDesktopMatches = window.location.href.match(/[?&]isScratchDesktop=([^&]+)/);
   var simulateScratchDesktop;
 
@@ -99,6 +101,7 @@ var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
     canEditTitle: true,
     isScratchDesktop: true,
     showTelemetryModal: true,
+    isTotallyNormal: isTotallyNormal,
     canSave: false,
     onTelemetryModalCancel: handleTelemetryModalCancel,
     onTelemetryModalOptIn: handleTelemetryModalOptIn,
@@ -107,6 +110,7 @@ var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
     canEditTitle: true,
     backpackVisible: true,
     showComingSoon: true,
+    isTotallyNormal: isTotallyNormal,
     backpackHost: backpackHost,
     canSave: false,
     onClickLogo: onClickLogo
@@ -119,7 +123,7 @@ var handleTelemetryModalOptOut = function handleTelemetryModalOptOut() {
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(1359);
+var content = __webpack_require__(1360);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -141,18 +145,18 @@ if(false) {}
 
 /***/ }),
 
-/***/ 955:
+/***/ 956:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(956);
+/* harmony import */ var es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(957);
 /* harmony import */ var es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(es6_object_assign_auto__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(958);
+/* harmony import */ var core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(959);
 /* harmony import */ var core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_fn_array_includes__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(970);
+/* harmony import */ var core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(971);
 /* harmony import */ var core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(core_js_fn_promise_finally__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var intl__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(987);
+/* harmony import */ var intl__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(988);
 /* harmony import */ var intl__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(intl__WEBPACK_IMPORTED_MODULE_3__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(0);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_4__);
@@ -186,7 +190,7 @@ document.body.appendChild(appTarget);
 if (Object(_lib_supported_browser__WEBPACK_IMPORTED_MODULE_9__[/* default */ "a"])()) {
   // require needed here to avoid importing unsupported browser-crashing code
   // at the top level
-  __webpack_require__(1360).default(appTarget);
+  __webpack_require__(1361).default(appTarget);
 } else {
   _components_browser_modal_browser_modal_jsx__WEBPACK_IMPORTED_MODULE_8__[/* default */ "a"].setAppElement(appTarget);
   var WrappedBrowserModalComponent = Object(_lib_app_state_hoc_jsx__WEBPACK_IMPORTED_MODULE_7__[/* default */ "a"])(_components_browser_modal_browser_modal_jsx__WEBPACK_IMPORTED_MODULE_8__[/* default */ "a"], true
@@ -203,12 +207,12 @@ if (Object(_lib_supported_browser__WEBPACK_IMPORTED_MODULE_9__[/* default */ "a"
 
 /***/ }),
 
-/***/ 989:
+/***/ 990:
 /***/ (function(module, exports) {
 
 /* (ignored) */
 
 /***/ })
 
-},[[955,0]]]);
+},[[956,0]]]);
 //# sourceMappingURL=gui.js.map

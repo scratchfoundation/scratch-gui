10de159ec13e4ea7245c6842ef3090d2a4c303d5
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2023-08-10 07:42:56 -0700

    test: provide consistent defaults for IntlProvider

--- test/helpers/intl-helpers.jsx
+++ test/helpers/intl-helpers.jsx
@@ -1,37 +1,54 @@
 /*
  * Helpers for using enzyme and react-test-renderer with react-intl
- * Directly from https://github.com/yahoo/react-intl/wiki/Testing-with-React-Intl
  */
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {IntlProvider} from 'react-intl';
-import intlShape from '../../src/lib/intl-shape';
+import {createIntl, IntlProvider} from 'react-intl';
 import {mount, shallow} from 'enzyme';
+import intlShape from '../../src/lib/intl-shape';
 
-const intlProvider = new IntlProvider({locale: 'en'}, {});
-const {intl} = intlProvider.getChildContext();
+/**
+ * @type {import('react-intl').IntlConfig}
+ */
+const defaultIntlConfig = Object.freeze({
+    locale: 'en',
+    messages: {},
+    textComponent: 'span'
+});
 
-const nodeWithIntlProp = node => React.cloneElement(node, {intl});
+const shallowWithIntl = (node, {context} = {}) =>
+    shallow(
+        node,
+        {
+            context: Object.assign({}, context),
+            wrappingComponent: IntlProvider,
+            wrappingComponentProps: defaultIntlConfig
+        }
+    ).dive();
 
-const shallowWithIntl = (node, {context} = {}) => shallow(
-    nodeWithIntlProp(node),
-    {
-        context: Object.assign({}, context, {intl})
-    }
-);
-
-const mountWithIntl = (node, {context, childContextTypes} = {}) => mount(
-    nodeWithIntlProp(node),
-    {
-        context: Object.assign({}, context, {intl}),
-        childContextTypes: Object.assign({}, {intl: intlShape}, childContextTypes)
-    }
-);
+const mountWithIntl = (node, {context, childContextTypes} = {}) => {
+    const intl = createIntl(defaultIntlConfig);
+    return mount(
+        node,
+        {
+            context: Object.assign({}, context, {intl}),
+            childContextTypes: Object.assign({}, {intl: intlShape}, childContextTypes),
+            wrappingComponent: IntlProvider,
+            wrappingComponentProps: defaultIntlConfig
+        }
+    );
+};
 
 // react-test-renderer component for use with snapshot testing
-const componentWithIntl = (children, props = {locale: 'en'}) => renderer.create(
-    <IntlProvider {...props}>{children}</IntlProvider>
-);
+const componentWithIntl = (children, props = {}) =>
+    renderer.create(
+        <IntlProvider
+            {...defaultIntlConfig}
+            {...props}
+        >
+            {children}
+        </IntlProvider>
+    );
 
 export {
     componentWithIntl,

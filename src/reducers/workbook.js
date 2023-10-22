a5357497319db43ebca0cb3285a03fa0a3f4fc1e
Kohei Matsumoto <baberugaberiimeronnv5@gmail.com>
2023-10-22 17:42:10 +0900

    feat: change sample questions (#14)

--- src/reducers/workbook.js
+++ src/reducers/workbook.js
@@ -3,30 +3,30 @@ const SET_NEXT_QUESTION = 'workbook/SET_NEXT_QUESTION';
 
 const QUESTIONS = [
     {
-        question: '正三角形をかいてください。',
+        question: '正三角形をかいてみよう。',
         toolboxBlocks: {
             motion: { movesteps: true, turnright: true, gotoxy: true },
             control: { repeat: true },
             pen: { clear: true, penDown: true, penUp: true },
         },
-        explanation: '正多角形は、辺の長さが全て等しく、角の大きさが全て等しい図形です。...'
+        explanation: '正三角形は、３本の辺の長さがすべておなじで、３つの角の大きさがすべて６０度の図形です。「長さ１００すすむ（線をひく）」、「左に１２０度まがる」を３回くり返すと正三角形がかけます。'
     },
     {
-        question: 'ねこをみぎに１０ぽ、うごかしてください。',
-        toolboxBlocks: {
-            motion: { movesteps: true, turnright: true, pointindirection: true },
-        },
-        explanation: 'もんだい１のせつめいです。ねこをみぎに１０ぽ、うごかせば、もんだいをとくことができます。',
-    },
-    {
-        question: '正八角形をかいてください。',
+        question: '正四角形をかいてみよう。',
         toolboxBlocks: {
             motion: { movesteps: true, turnright: true, setx: true, sety: true },
             control: { repeat: true },
             pen: { clear: true, penDown: true, penUp: true },
         },
-        explanation: '正多角形は、辺の長さが全て等しく、角の大きさが全て等しい図形です。...'
-    },
+        explanation: '正三角形は、４本の辺の長さがすべておなじで、４つの角の大きさがすべて９０度の図形です。「長さ１００すすむ（線をひく）」、「左に９０度まがる」を４回くり返すと正四角形がかけます。'
+    }, 
+    // {
+    //     question: 'ねこをみぎに１０ぽ、うごかしてください。',
+    //     toolboxBlocks: {
+    //         motion: { movesteps: true, turnright: true, pointindirection: true },
+    //     },
+    //     explanation: 'もんだい１のせつめいです。ねこをみぎに１０ぽ、うごかせば、もんだいをとくことができます。',
+    // },
 ]
 
 const initialState = {

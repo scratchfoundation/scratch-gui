dd98dd843fe55baaa555bfd6c15149883e80e896
Aaron Dodson <adodson@google.com>
2024-04-26 11:22:41 -0700

    fix: patch the getCheckboxState method in the new flyout (#7)

--- src/lib/blocks.js
+++ src/lib/blocks.js
@@ -328,7 +328,7 @@ export default function (vm, useCatBlocks) {
         this.jsonInit(json);
     };
 
-    ScratchBlocks.VerticalFlyout.getCheckboxState = function (blockId) {
+    ScratchBlocks.CheckableContinuousFlyout.prototype.getCheckboxState = function (blockId) {
         const monitoredBlock = vm.runtime.monitorBlocks._blocks[blockId];
         return monitoredBlock ? monitoredBlock.isMonitored : false;
     };

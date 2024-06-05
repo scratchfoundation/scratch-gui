8bebab9590141bb5695ffe389c5b8443cca073c5
vaporz <zzxx513@gmail.com>
2024-06-05 15:37:01 +0800

    fix: change title

--- webpack.config.js
+++ webpack.config.js
@@ -138,7 +138,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch 3.0 GUI',
+                title: '勘星科技-图形化编程',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({

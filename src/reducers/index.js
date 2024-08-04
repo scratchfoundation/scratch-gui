de6867511cd4eaa5426730b837f52af8c1124c2c
codingpioneer <60292577+doopyo85@users.noreply.github.com>
2024-08-05 03:41:39 +0900

    Update index.js
    리듀셔 추가

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux';
 import authReducer from './auth';
+import sessionReducer from './session'; // 새로 추가된 세션 리듀서
 import otherReducers from './otherReducers';
 
 export default combineReducers({
     auth: authReducer,
+    session: sessionReducer, // 새로 추가된 세션 리듀서
     ...otherReducers
 });
-

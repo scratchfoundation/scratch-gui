0ab8c929a6411df27d6dccfc6b92c2d29c75fc08
codingpioneer <60292577+doopyo85@users.noreply.github.com>
2024-08-08 21:48:50 +0900

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,12 @@ import { combineReducers } from 'redux';
 import authReducer from './auth';
 import sessionReducer from './session'; // 새로 추가된 세션 리듀서
 import otherReducers from './otherReducers';
+import menus from './menus';
+
+const rootReducer = combineReducers({
+    menus,
+    // 다른 리듀서 추가 가능
+});
 
 export default combineReducers({
     auth: authReducer,

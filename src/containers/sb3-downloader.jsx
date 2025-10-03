17753f257fd70bef36fa619cfa0490128e6d0a35
Raja Sekhar <rajshekhar.k321@gmail.com>
2025-10-03 10:04:34 +0530

    state refersh

--- src/containers/sb3-downloader.jsx
+++ src/containers/sb3-downloader.jsx
@@ -163,6 +163,22 @@ class SB3Downloader extends React.Component {
             new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ''),
           )
           this.props.setIsScratchData(base64blocks)
+          
+          // Brilliant solution: Re-run entire data capture process to get fresh VM state
+          setTimeout(() => {
+            this.props.saveProjectSb3().then((freshContent) => {
+              const freshReader = new FileReader()
+              freshReader.onloadend = async () => {
+                const freshBuffer = freshReader.result
+                let freshBase64blocks = btoa(
+                  new Uint8Array(freshBuffer).reduce((data, byte) => data + String.fromCharCode(byte), ''),
+                )
+                // This should now contain fresh VM state!
+                this.props.setIsScratchData(freshBase64blocks)
+              }
+              freshReader.readAsArrayBuffer(freshContent)
+            })
+          }, 50) // Small delay to allow VM to update its state
         }
         reader.readAsArrayBuffer(content)
       })
